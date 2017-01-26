50007c6dcb80261d46c0a014d6733e8838e41aa8
techbot <techbot121@gmail.com>
2017-01-26 17:12:10 +0100

    add resources.lua

---
+++ Resources/lua/autorun/server/resources.lua
@@ -0,0 +1,5 @@
+resource.AddWorkshop("243902601") -- gm_bluehills_test3 map
+resource.AddWorkshop("108168164") -- Customisable Thirdperson
+resource.AddWorkshop("108170491") -- Wowozela
+resource.AddWorkshop("109624491") -- Vuvuzela
+resource.AddWorkshop("546392647") -- Media Player
