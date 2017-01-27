bbfa25f2e847affdcc14dfc4ed5bfc7b322ab6dc
techbot <techbot@threekelv.in>
2017-01-27 23:56:57 +0100

    Delete spooky folder which windows doesnt detect

--- Resources/lua/autorun/server/resources.lua
+++
@@ -1,6 +0,0 @@
-resource.AddWorkshop("243902601") -- gm_bluehills_test3 map
-resource.AddWorkshop("108168164") -- Customisable Thirdperson
-resource.AddWorkshop("108170491") -- Wowozela
-resource.AddWorkshop("109624491") -- Vuvuzela
-resource.AddWorkshop("546392647") -- Media Player
-resource.AddWorkshop("129640520") -- JRPG Hitmarks
