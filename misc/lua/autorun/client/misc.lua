2b5f192b6f1ef8182e293e8f89d0a10e5cca4b3c
techbot <techbot121@gmail.com>
2017-01-28 22:50:09 +0100

    add hostname fix

--- misc/lua/autorun/client/misc.lua
+++ misc/lua/autorun/client/misc.lua
@@ -1,3 +1,8 @@
 timer.Simple(0, function()
 	RunConsoleCommand("r_radiosity", "1") -- this is gonna be default in the next update
 end)
+
+-- Hostnamefix
+function GetHostName()
+	return GetGlobalString("ServerName")
+end
