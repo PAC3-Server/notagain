0eac573347cf894ccd735779933072276c79a109
Elias Hogstvedt <eliashogstvedt@gmail.com>
2017-01-30 09:21:34 +0100

    r_radiosity 4 not r_radiosity 1..

--- misc/lua/autorun/client/misc.lua
+++ misc/lua/autorun/client/misc.lua
@@ -1,5 +1,5 @@
 timer.Simple(0, function()
-	RunConsoleCommand("r_radiosity", "1") -- this is gonna be default in the next update
+	RunConsoleCommand("r_radiosity", "4") -- this is gonna be default in the next update
 end)
 
 -- Hostnamefix
