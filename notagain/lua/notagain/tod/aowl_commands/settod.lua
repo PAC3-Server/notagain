e124c078456786c87e4f8ebdf77e443fa47a1a64
techbot <techbot121@gmail.com>
2017-01-31 11:58:10 +0100

    more tod fixes

--- notagain/lua/notagain/tod/aowl_commands/settod.lua
+++ notagain/lua/notagain/tod/aowl_commands/settod.lua
@@ -2,12 +2,12 @@ aowl.AddCommand("settod", function(player, line, val)
 	local val = val or "realtime"
 	local time24 = tonumber(val)
 
-	if time24 then
+	if time24 and time24 > 0 then
 		RunConsoleCommand("sv_tod", "0")
 		tod.SetCycle((time24 / 24)%1)
 	elseif val == "demo" then
 		RunConsoleCommand("sv_tod", "2")
-	elseif val == "realtime" or time24 < 0 then
+	else
 		RunConsoleCommand("sv_tod", "1")
 	end
 
