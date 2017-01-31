0d43f123022963bd6a841013b47329988904aff6
CapsAdmin <eliashogstvedt@gmail.com>
2017-01-31 21:20:40 +0100

    fix IsMuted returning number

--- notagain/lua/notagain/essential/autorun/client/perma_client_mute.lua
+++ notagain/lua/notagain/essential/autorun/client/perma_client_mute.lua
@@ -3,7 +3,7 @@ META.Old_IsMuted = META.Old_IsMuted or META.IsMuted
 META.Old_SetMuted = META.Old_SetMuted or META.SetMuted
 
 function META:IsMuted()
-	return self:GetPData("perma_voice_mute", self:Old_IsMuted() and 1 or 0)
+	return self:GetPData("perma_voice_mute", self:Old_IsMuted() and 1 or 0) == 1
 end
 
 function META:SetMuted(b)
