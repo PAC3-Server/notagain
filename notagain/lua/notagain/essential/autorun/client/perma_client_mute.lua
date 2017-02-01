840b06ca326c67f763e3ee1d775a64335dd38385
CapsAdmin <eliashogstvedt@gmail.com>
2017-02-01 02:03:19 +0100

    fix mute

--- notagain/lua/notagain/essential/autorun/client/perma_client_mute.lua
+++ notagain/lua/notagain/essential/autorun/client/perma_client_mute.lua
@@ -1,11 +1,17 @@
 local META = FindMetaTable("Player")
+
 META.Old_IsMuted = META.Old_IsMuted or META.IsMuted
 META.Old_SetMuted = META.Old_SetMuted or META.SetMuted
 
 function META:IsMuted()
-	return self:GetPData("perma_voice_mute", self:Old_IsMuted() and 1 or 0) == 1
+	return self:GetPData("perma_voice_mute", self:Old_IsMuted() and "1" or "0") == "1"
 end
 
 function META:SetMuted(b)
-	self:SetPData("perma_voice_mute", b and 1 or 0)
+	self:SetPData("perma_voice_mute", b and "1" or "0")
+	self:Old_SetMuted(b)
 end
+
+hook.Add("PlayerStartVoice", "perma_voice_mute", function(ply)
+	ply:SetMuted(ply:IsMuted())
+end)
