05da4431e148eafa2473886411bff6b1ec712c78
CapsAdmin <eliashogstvedt@gmail.com>
2017-02-03 17:33:10 +0100

    unlimited ammo

---
+++ notagain/lua/notagain/pac_server/autorun/server/unlimited_ammo.lua
@@ -0,0 +1,29 @@
+timer.Create("unlimited_ammo", 0.25, 0, function()
+	for _, ply in pairs(player.GetAll()) do
+		local wep = ply:GetActiveWeapon()
+
+		if wep:IsValid() then
+			local max = wep:GetMaxClip1()
+			if wep:Clip1() < max then
+				wep:SetClip1(max)
+			end
+
+			local max = wep:GetMaxClip2()
+			if wep:Clip2() < max then
+				wep:SetClip2(max)
+			end
+
+			local id = wep:GetPrimaryAmmoType()
+			local max = game.GetAmmoMax(id)
+			if ply:GetAmmoCount(id) < max then
+				ply:SetAmmo(max, id)
+			end
+
+			local id = wep:GetSecondaryAmmoType()
+			local max = game.GetAmmoMax(id)
+			if ply:GetAmmoCount(id) < max then
+				ply:SetAmmo(max, id)
+			end
+		end
+	end
+end)
