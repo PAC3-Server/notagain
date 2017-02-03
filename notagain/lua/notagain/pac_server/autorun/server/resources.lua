4535a90e22418b0f28396a78fb51de155a0e4356
CapsAdmin <eliashogstvedt@gmail.com>
2017-02-03 15:29:37 +0100

    ops

--- notagain/lua/notagain/pac_server/autorun/server/resources.lua
+++ notagain/lua/notagain/pac_server/autorun/server/resources.lua
@@ -2,8 +2,8 @@ resource.AddWorkshop("546392647") -- Media Player
 
 do
 	local map_content = {
-		ze_ffvii_mako_reactor_v5_3 = {"307755108"}
-		gm_bluehills_test3 = {"243902601"}
+		ze_ffvii_mako_reactor_v5_3 = {"307755108"},
+		gm_bluehills_test3 = {"243902601"},
 	}
 
 	local map = game.GetMap():lower()
