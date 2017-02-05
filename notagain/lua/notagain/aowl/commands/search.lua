e5bd7f83d279a2bbf488d52e778ca65fc41fe8cc
YaraHub <reno.reboursier@gmail.com>
2017-02-05 12:57:05 +0100

    Search

---
+++ notagain/lua/notagain/aowl/commands/search.lua
@@ -0,0 +1,22 @@
+AddCSLuaFile()
+
+local Tag="search"
+
+if SERVER then
+	util.AddNetworkString(Tag)
+	
+	aowl.AddCommand({"g","search"}, function( player , line , search )
+		if search then
+			net.Start(Tag)
+			net.WriteString(search)
+			net.Send(player)
+		end
+	end)
+end
+
+if CLIENT then
+	net.Receive(Tag,function()
+		local parts = string.Explode(" ",net.ReadString())
+		gui.OpenURL("https://www.google.co.uk/#q="..table.concat( parts, "+", 1, #parts ))
+	end)
+end
