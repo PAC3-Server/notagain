4e3880abc772a2a419b6552bd14ac1b8c1eeadcd
YaraHub <reno.reboursier@gmail.com>
2017-02-05 13:55:22 +0100

    thirdperson

---
+++ notagain/lua/notagain/aowl/commands/thirdperson.lua
@@ -0,0 +1,24 @@
+AddCSLuaFile()
+
+local Tag="thirdperson"
+
+if SERVER then
+	util.AddNetworkString(Tag)
+	
+	aowl.AddCommand({"ctp","thirdperson","view"}, function( player , line  )
+		if IsValid(player) then
+			net.Start(Tag)
+			net.Send(player)
+		end
+	end)
+end
+
+if CLIENT then
+	net.Receive(Tag,function()
+		if ctp.Enabled then
+			ctp.Disable()
+		else
+			ctp.Enable()
+		end
+	end)
+end
