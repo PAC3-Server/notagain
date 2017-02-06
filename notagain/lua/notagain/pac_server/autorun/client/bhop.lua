cc3cdddced933d590603482afe1e04c1ed0faaf5
YaraHub <reno.reboursier@gmail.com>
2017-02-06 12:12:28 +0100

    bhop disabled by default check auto_bhop convar

---
+++ notagain/lua/notagain/pac_server/autorun/client/bhop.lua
@@ -0,0 +1,12 @@
+local bhop_convar = CreateClientConVar( "auto_bhop", "0", true, false )
+
+hook.Add("CreateMove","bizzahop",function( cmd )
+	if bhop_convar:GetInt() == 1 then
+		if bit.band( cmd:GetButtons() , IN_JUMP ) != 0 then
+			if !LocalPlayer():IsOnGround() then
+				cmd:SetButtons( bit.band( cmd:GetButtons(), bit.bnot( IN_JUMP ) ) )
+
+			end
+		end
+	end
+end)
