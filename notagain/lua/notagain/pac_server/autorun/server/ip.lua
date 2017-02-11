2c655c0d6abb1705d93811a5da885634255ad58f
techbot <techbot121@gmail.com>
2017-02-11 16:23:37 +0100

    didn't even know this existed

--- notagain/lua/notagain/pac_server/autorun/server/ip.lua
+++ notagain/lua/notagain/pac_server/autorun/server/ip.lua
@@ -1,8 +1,4 @@
-gameevent.Listen("player_connect")
-hook.Add("player_connect","SetIPAddress",function(data)
-    util.SetPData(data.networkid,"IP",string.Split(data.address,":")[1]) -- well it works
-end)
-
 FindMetaTable("Player").IP = function(ply)
-    return ply:GetPData("IP",nil)
+    if not IsValid(ply) then return end
+    return string.Split(ply:IPAddress(),":"[1])
 end
