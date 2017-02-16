073454cd824b28f73bf2fbf923c0e30cb58a4110
CapsAdmin <eliashogstvedt@gmail.com>
2017-02-16 19:47:14 +0100

    add gm_abstraction_extende

--- notagain/lua/notagain/pac_server/autorun/server/workshop_resources.lua
+++ notagain/lua/notagain/pac_server/autorun/server/workshop_resources.lua
@@ -4,6 +4,7 @@ do
 	local map_content = {
 		ze_ffvii_mako_reactor_v5_3 = {"307755108"},
 		gm_bluehills_test3 = {"243902601"},
+		gm_abstraction_extended = {"734919940"},
 	}
 
 	local map = game.GetMap():lower()
