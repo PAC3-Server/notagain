6bd48ffaa26cad3aec26fce1769bd7060550cae9
YaraHub <reno.reboursier@gmail.com>
2017-02-18 14:59:13 +0100

    so players can use space while having it on

--- notagain/lua/notagain/pac_server/autorun/client/auto_bhop.lua
+++ notagain/lua/notagain/pac_server/autorun/client/auto_bhop.lua
@@ -3,7 +3,7 @@ local bhop_convar = CreateClientConVar( "auto_bhop", "0", true, false )
 hook.Add("CreateMove","bizzahop",function( cmd )
 	if bhop_convar:GetInt() == 1 then
 		if bit.band( cmd:GetButtons() , IN_JUMP ) != 0 then
-			if !LocalPlayer():IsOnGround() then
+			if !LocalPlayer():IsOnGround() and LocalPlayer():GetMoveType() != MOVETYPE_NOCLIP  then
 				cmd:SetButtons( bit.band( cmd:GetButtons(), bit.bnot( IN_JUMP ) ) )
 
 			end
