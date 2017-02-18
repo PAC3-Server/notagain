ff3193c6f8c579210544c822724f7db96fd3654b
Toasty Engineer <timrace01@gmail.com>
2017-02-18 22:38:06 +0100

    Update join_leave_messages.lua
    this should be better now

--- notagain/lua/notagain/pac_server/autorun/server/join_leave_messages.lua
+++ notagain/lua/notagain/pac_server/autorun/server/join_leave_messages.lua
@@ -1,42 +1,86 @@
+
+
 local tag = "player_info"
-pcall(require,"geoip")
 
-local function JoinMessage(name,steamid)
-	for _,v in next,player.GetAll() do 
-		v:SendLua([[chat.AddText(Color(255,255,255),"]] .. name .. [[ (]] .. steamid .. [[) is ",Color(0,255,0),"connecting",Color(255,255,255)," to the server!")]])
+if SERVER then
+
+	util.AddNetworkString(tag)
+
+	local geoip = requirex("geoip")
+
+	local function JoinMessage(name,steamid)
+		local info = {}
+		info.name = name
+		info.steamid = steamid
+		net.Start(tag)
+			net.WriteTable(info)
+		net.Broadcast()
 	end
+
+	local function LeaveMessage(name,steamid,reason)
+		local info = {}
+		info.name = name
+		info.steamid = steamid
+		info.reason = reason
+		net.Start(tag)
+			net.WriteTable(info)
+		net.Broadcast()
+	end
+
+
+	gameevent.Listen("player_connect")
+	hook.Add("player_connect",tag,function(data)
+		local name = data.name
+		local ip = data.address
+		local steamid = data.networkid
+		local geoipres = GeoIP.Get(ip:Split(":")[1])
+		local geoipinfo = table.ClearKeys({country_name = geoipres.country_name or "no country?",city = geoipres.city or "no city?",asn = geoipres.asn or "no asn?"})
+
+		MsgC(Color(0,255,0),"[Join] ") print(name.." ("..steamid..") is connecting to the server! ["..ip..(geopipinfo and table.Count(geoipinfo) ~= 0 and " | "..table.concat(geoipinfo,", ").."]" or "]"))
+
+		JoinMessage(name,steamid)
+
+	end)
+
+	gameevent.Listen("player_disconnect")
+	hook.Add("player_disconnect",tag,function(data)
+		local name = data.name
+		local steamid = data.networkid
+		local reason = data.reason
+
+		LeaveMessage(name,steamid,reason)
+		
+	end)
+
+	function GAMEMODE:PlayerConnect() end
+	function GAMEMODE:PlayerDisconnected() end
+
 end
 
-local function LeaveMessage(name,steamid,reason)
-	for _,v in next,player.GetAll() do 
-		v:SendLua([[chat.AddText(Color(255,255,255),"]] .. name .. [[ (]] .. steamid .. [[) has ",Color(255,0,0),"disconnected",Color(255,255,255)," from the server! (]] .. reason .. [[)")]]) 
-	end
+if CLIENT then
+
+	net.Receive(tag,function()
+		local info = net.ReadTable()
+
+		if not info.reason then
+
+			chat.AddText(Color(255,255,255),info.name .. " (" .. info.steamid .. ") is ", Color(0,255,0), "connecting", Color(255,255,255), " to the server!")
+
+		else
+
+			chat.AddText(Color(255,255,255),info.name .. " (" .. info.steamid .. ") has ", Color(255,0,0), "disconnected", Color(255,255,255), " from the server! (" .. info.reason .. ")")
+
+		end
+
+	end)
+
+	hook.Add("ChatText",tag,function(some,very,special,shit)
+		if shit == "joinleave" then 
+			return true
+		end
+	end)
+
+
 end
 
 
-gameevent.Listen("player_connect")
-hook.Add("player_connect",tag,function(data)
-	local name = data.name
-	local ip = data.address
-	local steamid = data.networkid
-	local geoip = GeoIP.Get(ip:Split(":")[1])
-	local geoipinfo = {geoip.country_name,geoip.city,geoip.asn}
-	
-	MsgC(Color(0,255,0),"[Join] ") print(name.." ("..steamid..") is connecting to the server! ["..ip..(table.Count(geoipinfo) ~= 0 and " | "..table.concat(geoipinfo,", ").."]" or "]"))
-
-	JoinMessage(name,steamid)
-
-end)
-
-gameevent.Listen("player_disconnect")
-hook.Add("player_disconnect",tag,function(data)
-	local name = data.name
-	local steamid = data.networkid
-	local reason = data.reason
-
-	LeaveMessage(name,steamid,reason)
-	
-end)
-
-function GAMEMODE:PlayerConnect() end
-function GAMEMODE:PlayerDisconnected() end
