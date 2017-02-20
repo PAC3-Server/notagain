3cc74bd5402864ec7b46e2b237e1ed2e32d69a8f
CapsAdmin <eliashogstvedt@gmail.com>
2017-02-20 12:08:51 +0100

    improved bunny jump prediction and other things

--- notagain/lua/notagain/essential/autorun/bunny_jump.lua
+++ notagain/lua/notagain/essential/autorun/bunny_jump.lua
@@ -21,68 +21,47 @@ do
 	end
 end
 
-local function calc_wall(ply, data)
-	local velocity = data:GetVelocity()
-	local normalized = velocity:GetNormalized()
+local function play_effect(ply, fraction, origin, normal)
+	ply:EmitSound(
+		("weapons/fx/rics/ric%s.wav"):format(math.random(5)),
+		75,
+		math.Clamp(300*fraction, 70, 150),
+		math.Clamp(fraction*0.5, 0, 1)
+	)
+	ply:EmitSound(
+		("physics/plastic/plastic_barrel_impact_bullet3.wav"):format(math.random(5)),
+		75,
+		100,
+		math.Clamp(fraction, 0, 1)
+	)
 
-	local params = {}
-		params.start = ply:GetPos()+Vector(0,0,36)
-		params.endpos = params.start+(normalized*math.max(velocity:Length()/20, 50))
-		params.filter = ply
-		params.mins = ply:OBBMins()
-		params.maxs = ply:OBBMaxs()
-	local res = util.TraceHull(params)
+	local ef = EffectData()
+		ef:SetOrigin(origin)
+		ef:SetScale(10*fraction)
+		ef:SetNormal(normal)
+	util.Effect("StunstickImpact", ef)
 
-	if not ply:IsOnGround() and res.Hit and math.abs(res.HitNormal.z) < 0.7 then
-		local direction = velocity - 2 * (res.HitNormal:DotProduct(velocity) * res.HitNormal)
-
-		local fraction = math.min(velocity:Length() / GetConVarNumber("sv_maxvelocity"), 1)
-
-		ply:SetGroundEntity(NULL)
-		data:SetVelocity(direction*1.25)--*(fraction*2+(0.5)))
-
-		if SERVER then
-
-			local fraction = data:GetVelocity():Length() / (GetConVarNumber("sv_maxvelocity") / 4)
-			sound.Play(("weapons/fx/rics/ric%s.wav"):format(math.random(5)), data:GetOrigin(), math.Clamp(300*fraction, 20, 150), math.Clamp(fraction*255+70, 70, 150))
-			sound.Play(("weapons/crossbow/fire1.wav"):format(math.random(5)), data:GetOrigin(), 100, math.Clamp(fraction*255+120, 70, 255))
-
-		end
-	end
 end
 
-local function Move(ply, data)
-	local mult = ply:GetSuperJumpMultiplier()
+if CLIENT then
+	net.Receive("bhop_effect", function(len)
+		local ply = net.ReadEntity()
+		if ply:IsValid() then
+			local fraction = net.ReadFloat()
+			local origin = net.ReadVector()
+			local normal = net.ReadVector()
 
-	if ply:KeyPressed(IN_JUMP) and ply:GetMoveType() == MOVETYPE_WALK then
-		if mult ~= 1 and ply:IsOnGround() then
-			data:SetVelocity(data:GetVelocity() * mult)
-
-			local eye = math.Clamp(ply:EyeAngles().p/89, 0, 1) ^ 3
-			if eye > 0.3 then
-
-				if SERVER then
-					local fraction = data:GetVelocity():Length() / (GetConVarNumber("sv_maxvelocity") / 4)
-					sound.Play(("weapons/fx/rics/ric%s.wav"):format(math.random(5)), data:GetOrigin(), math.Clamp(300*fraction, 20, 150), math.Clamp(fraction*255+70, 70, 150))
-					sound.Play(("physics/plastic/plastic_barrel_impact_bullet3.wav"):format(math.random(5)), data:GetOrigin(), 100, math.Clamp(fraction*255+40, 70, 255))
-
-					local ef = EffectData()
-						ef:SetOrigin(data:GetOrigin())
-						ef:SetScale(10)
-					timer.Simple(0, function() util.Effect("StunstickImpact", ef) end)
-				end
-
-				data:SetVelocity(LerpVector(eye, data:GetVelocity(), Vector(0.5, 0.5, 1) * data:GetVelocity() + Vector(0,0,data:GetVelocity():Length()*0.3)))
-			end
-
+			play_effect(ply, fraction, origin, normal)
 		end
+	end)
 
-		calc_wall(ply, data)
-	end
+	usermessage.Hook("bhop", function(u)
+		LocalPlayer():SetSuperJumpMultiplier(u:ReadFloat())
+	end)
 end
 
 if SERVER then
-	hook.Add("SetupMove", "bhop", Move)
+	util.AddNetworkString("bhop_effect")
 
 	hook.Add("GetFallDamage", "bhop", function(ply, speed)
 		if ply:KeyDown(IN_JUMP) then
@@ -91,15 +70,86 @@ if SERVER then
 	end)
 end
 
-if CLIENT then
-	hook.Add("Move", "bhop", Move)
+local function send_effect(ply, fraction, origin, normal)
+	if CLIENT then
+		if IsFirstTimePredicted() then
+			play_effect(ply, fraction, origin, normal)
+		end
+	end
 
-	usermessage.Hook("bhop", function(u)
-		LocalPlayer():SetSuperJumpMultiplier(u:ReadFloat())
-	end)
+	if SERVER then
+		net.Start("bhop_effect", true)
+			net.WriteEntity(ply)
+			net.WriteFloat(fraction)
+			net.WriteVector(origin)
+			net.WriteVector(normal)
+		net.SendOmit(ply)
+	end
 end
 
-if SERVER then
-	RunConsoleCommand("sv_airaccelerate", "1000000")
-	RunConsoleCommand("sv_maxvelocity", "20000")
-end
+hook.Add("Move", "bhop", function(ply, data)
+	if ply:GetMoveType() ~= MOVETYPE_WALK then return end
+
+	local mult = ply:GetSuperJumpMultiplier()
+
+	if mult ~= 1 and data:KeyPressed(IN_JUMP) and ply:IsOnGround() then
+		data:SetVelocity(data:GetVelocity() * mult)
+
+		local eye = math.Clamp(data:GetAngles().p / 89, 0, 1) ^ 3
+
+		if eye > 0.3 then
+			ply:SetGroundEntity(NULL)
+
+			local fraction = data:GetVelocity():Length() / (GetConVarNumber("sv_maxvelocity") / 4)
+
+			send_effect(ply, fraction, data:GetOrigin(), Vector(0,0,1))
+
+			data:SetVelocity(LerpVector(eye, data:GetVelocity(), Vector(0.5, 0.5, 1) * data:GetVelocity() + Vector(0,0,data:GetVelocity():Length()*0.3)) + Vector(0,0, ply:GetJumpPower()))
+		end
+	end
+
+	if data:KeyPressed(IN_JUMP) or data:KeyWasDown(IN_JUMP) then
+		local velocity = data:GetVelocity()
+		local normalized = velocity:GetNormalized()
+
+		local params = {}
+			params.start = data:GetOrigin()
+			params.endpos = params.start + normalized * ply:BoundingRadius() * 2
+			params.filter = ply
+			params.mins = ply:OBBMins()
+			params.maxs = ply:OBBMaxs()
+		local res = util.TraceHull(params)
+
+		if
+			not ply:IsOnGround() and
+			res.Hit and
+			math.abs(res.HitNormal.z) < 0.7 and
+			(not ply.bhop_wall_bounce_origin or ply.bhop_wall_bounce_origin:Distance(data:GetOrigin()) > ply:BoundingRadius()) and
+			velocity.x ~= 0 and velocity.y ~= 0
+		then
+			local direction = velocity - 2 * (res.HitNormal:DotProduct(velocity) * res.HitNormal)
+
+			local fraction = math.min(velocity:Length() / GetConVarNumber("sv_maxvelocity"), 1)
+
+			data:SetVelocity(direction*1.1)
+
+			local fraction = velocity:Length() / (GetConVarNumber("sv_maxvelocity") / 4)
+
+			send_effect(ply, fraction, res.HitPos, res.HitNormal)
+
+			ply.bhop_wall_bounce_origin = data:GetOrigin()
+		end
+	end
+end)
+
+hook.Add("Initialize", "bhop", function()
+	function GAMEMODE:StartMove() end
+	function GAMEMODE:FinishMove() end
+
+	if SERVER then
+		RunConsoleCommand("sv_airaccelerate", "1000000")
+		RunConsoleCommand("sv_maxvelocity", "20000")
+	end
+
+	hook.Remove("Initialize","bhop")
+end)
