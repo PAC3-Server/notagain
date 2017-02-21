16bbae98ad17fd7c7b801045e0214d7946ab4ff8
Toasty Engineer <timrace01@gmail.com>
2017-02-21 20:54:37 +0100

    I dum dum
    ok this should finally do, did a last check

--- notagain/lua/notagain/pac_server/autorun/client/team_setup.lua
+++ notagain/lua/notagain/pac_server/autorun/client/team_setup.lua
@@ -1,5 +1,5 @@
 local players = 1
-local friends = 1
+local friends = 2
 
 team.SetUp(players, "players", Color(255, 70, 0, 255))
 team.SetUp(friends, "friends", Color(60, 127, 255, 255))
@@ -7,12 +7,9 @@ team.SetUp(friends, "friends", Color(60, 127, 255, 255))
 FindMetaTable("Player")._Team = FindMetaTable("Player")._Team or FindMetaTable("Player").Team
 
 FindMetaTable("Player").Team = function(self)
-	if not self:IsValid() or not LocalPlayer():IsValid() then return self:_Team() end
-
 	if self:GetFriendStatus() == "friend" or self == LocalPlayer() then
 		return friends
 	end
 
 	return players
-	
 end
