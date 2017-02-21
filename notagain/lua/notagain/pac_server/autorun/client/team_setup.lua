854529d24bdf34f90f8bd6619bb3f3209ff7049b
Toasty Engineer <timrace01@gmail.com>
2017-02-21 20:31:15 +0100

    Possibly preventing an error

--- notagain/lua/notagain/pac_server/autorun/client/team_setup.lua
+++ notagain/lua/notagain/pac_server/autorun/client/team_setup.lua
@@ -1,9 +1,18 @@
-team.SetUp(1, "players", Color(255, 70, 0, 255))
-team.SetUp(2, "friends", Color(60, 127, 255, 255))
+local players = 1
+local friends = 1
+
+team.SetUp(players, "players", Color(255, 70, 0, 255))
+team.SetUp(friends, "friends", Color(60, 127, 255, 255))
+
+FindMetaTable("Player")._Team = FindMetaTable("Player")._Team or FindMetaTable("Player").Team
 
 FindMetaTable("Player").Team = function(self)
+	if not self:IsValid() or not LocalPlayer():IsValid() then return self:_Team() end
+
 	if self:GetFriendStatus() == "friend" or self == LocalPlayer() then
-		return 2
+		return friends
 	end
-	return 1
+
+	return players
+	
 end
