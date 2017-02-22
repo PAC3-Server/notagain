66007cf4b2198ae51ef7b4075f7e39cee18424ca
CapsAdmin <eliashogstvedt@gmail.com>
2017-02-22 01:09:54 +0100

    fix isfriend
    ?

--- notagain/lua/notagain/pac_server/autorun/IsFriend.lua
+++ notagain/lua/notagain/pac_server/autorun/IsFriend.lua
@@ -1,79 +1,55 @@
 local Tag = "FriendSystem"
 
 if SERVER then
-	AddCSLuaFile()
-	local META = FindMetaTable( "Player" )
-	
-	util.AddNetworkString( Tag )
+	local META = FindMetaTable("Player")
 
-	function META:AddFriend( ply )
+	util.AddNetworkString(Tag)
 
-		if IsValid( ply ) and ply:IsPlayer() then
+	function META:AddFriend(ply)
+		if IsValid(ply) and ply:IsPlayer() then
 			self.Friends = self.Friends or {}
-			table.insert( self.Friends , ply:EntIndex() , ply )
+			self.Friends[ply] = ply
 		end
-
 	end
 
-	function META:RemoveFriend( ply )
-
-		if IsValid( ply ) and ply:IsPlayer() and self.Friends then
-			table.remove( self.Friends ,ply:EntIndex() )
+	function META:RemoveFriend(ply)
+		if IsValid(ply) and ply:IsPlayer() and self.Friends then
+			self.Friends[ply] = nil
 		end
-
 	end
 
-	net.Receive( Tag , function( len , ply )
-		local NWFriends = net.ReadTable()
-		
-		for _ , v in pairs( NWFriends ) do
-			v:AddFriend( ply )
-			ply:AddFriend( v )
-		end 
-
-	end )
-
-	hook.Add( "PlayerDisconnected" , Tag , function( ply )
-
-		for _ , v in pairs( player.GetAll() ) do
-			v:RemoveFriend( ply )
-		end
-
-	end )
-
-	function META:IsFriend( ply ) 
-
-		if IsValid( ply ) and ply:IsPlayer() then
-
-			for k,v in pairs( self.Friends ) do
-				if v == ply then
-					return true
-				end
-			end
-
-			return false
-
-		end
-
+	function META:IsFriend(ply)
+		return self.Friends and self.Friends[ply] ~= nil
 	end
 
+	net.Receive(Tag , function(len , ply)
+		if not ply:IsValid() then return end
+
+		local friend = net.ReadEntity()
+		if friend:IsValid() then
+			friend:AddFriend(ply)
+			ply:AddFriend(friend)
+		end
+	end)
+
+	hook.Add("PlayerDisconnected" , Tag, function(friend)
+		for _ , ply in ipairs(player.GetAll()) do
+			ply:RemoveFriend(friend)
+		end
+	end)
 end
 
-
 if CLIENT then
+	hook.Add("OnEntityCreated", Tag, function(ply)
+		-- might be too early if the player has just spawned?
+		timer.Simple(1, function()
+			if not ply:IsValid() or not ply:IsPlayer() then return end
 
-	local Friends = Friends or {}
-	
-	for _ , v in pairs( player.GetAll() ) do
-		if v:GetFriendStatus() == "friend" then
-			table.insert( Friends , v:EntIndex() , v )
-		end
-	end
-
-	net.Start( Tag )
-	net.WriteTable( Friends )
-	net.SendToServer()
-
-
-
+			if ply:GetFriendStatus() == "friend" then
+				net.Start(Tag)
+				net.WriteEntity(ply)
+				net.SendToServer()
+			end
+		end)
+	end)
 end
