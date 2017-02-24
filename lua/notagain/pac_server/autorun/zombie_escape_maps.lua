7aa855248f27217f84d95263bc37023525ad002c
Yara <reno.reboursier@gmail.com>
2017-02-24 14:42:21 +0100

    Fix doors in ze maps

--- lua/notagain/pac_server/autorun/zombie_escape_maps.lua
+++ lua/notagain/pac_server/autorun/zombie_escape_maps.lua
@@ -11,22 +11,12 @@ hook.Add("InitPostEntity", "fixmap", function()
 			func_wall_toggle = true,
 			func_clip_vphysics = true,
 		}
-			
-		local open_these = {
-			func_door = true,
-			func_door_rotating = true,
-			func_movelinear = true,
-		}
 
 		for _, ent in pairs(ents.GetAll()) do
 			local class = ent:GetClass()
 
 			if remove_these[ent:GetClass()] or ent:GetClass():match("trigger_*") then
 				ent:Remove()
-			end
-			
-			if open_these[ent:GetClass()] then
-				ent:Fire("Open")
 			end
 		end
 	end
@@ -37,3 +27,23 @@ hook.Add("InitPostEntity", "fixmap", function()
 
 	hook.Remove("InitPostEntity", "fixmap")
 end)
+
+if SERVER then
+	
+	hook.Add( "KeyPress", "OpenDoors", function( ply, key )
+		if ( key == IN_USE ) then
+
+			local ToOpen = {
+				func_door = true,
+				func_door_rotating = true,
+				func_movelinear = true,
+			}
+			local tr = ply:GetEyeTrace()
+
+			if ToOpen[tr.Entity:GetClass()] and tr.HitPos:Distance(ply:GetPos()) <= 100 then
+				tr.Entity:Fire("Open")
+			end
+		end
+	end )
+	
+end
