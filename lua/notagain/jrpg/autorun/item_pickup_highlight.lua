ed466a134662c8180f40e1a6ffbaae38231cb298
CapsAdmin <eliashogstvedt@gmail.com>
2017-02-28 22:31:32 +0100

    improve effect in viewmodel and world model when being held

--- lua/notagain/jrpg/autorun/item_pickup_highlight.lua
+++ lua/notagain/jrpg/autorun/item_pickup_highlight.lua
@@ -219,11 +219,7 @@ if CLIENT then
 
 	local last_frame = 0
 
-	hook.Add("PrePlayerDraw", "jrpg_items", function(ply)
-		if ply.item_pickup_player_draw_hack then return true end
-	end)
-
-	local function draw_glow(ent, time, pos, distance, radius, vis, color, vm)
+	local function draw_glow(ent, time, distance, radius, vis, color, vm, wm)
 		ent.jrpg_items_random = ent.jrpg_items_random or {}
 		ent.jrpg_items_random.rotation = ent.jrpg_items_random.rotation or math_random()*360
 
@@ -233,12 +229,19 @@ if CLIENT then
 		if not vm then
 			cam_IgnoreZ(true)
 		end
+
+		local r = radius/8
+		local pos = ent:GetBonePosition(1) or ent:GetBonePosition(0)
+
+		if vm then
+			pos = vector_origin
+		end
+
 		render_DrawSprite(pos, 50, 50, TempColor(color.r*2, color.g*2, color.b*2, vis*20), ent.jrpg_items_random.rotation)
 
 		render_SetMaterial(glare2_mat)
 
 		local glow = math_sin(time*5)*0.5+0.5
-		local r = radius/8
 		render_DrawSprite(pos, r*10, r*10, TempColor(color.r, color.g, color.b, vis*170*glow))
 		render_DrawSprite(pos, r*20, r*20, TempColor(color.r, color.g, color.b, vis*170*(glow+0.25)))
 		render_DrawSprite(pos, r*30, r*30, TempColor(color.r, color.g, color.b, vis*120*(glow+0.5)))
@@ -247,86 +250,114 @@ if CLIENT then
 			cam_IgnoreZ(false)
 		end
 
+		render.ModelMaterialOverride(shiny)
+		render_SetColorModulation(color.r/200, color.g/200, color.b/200)
 		ent:DrawModel()
 
 		render_SetMaterial(glare_mat)
+
 		render_DrawSprite(pos, r*180, r*50, TempColor(color.r, color.g, color.b, vis*20))
 
 		if distance < 1500 then
 
 			if not ent.jrpg_items_next_emit2 or ent.jrpg_items_next_emit2 < time then
+				local emitted = 1
+				for _, atch in ipairs(ent:GetAttachments()) do
+					local pos = ent:GetAttachment(atch.id).Pos
 
-				local p = emitter2d:Add(glare2_mat, pos + (VectorRand()*radius*0.5))
-				p:SetDieTime(math.Rand(2,4))
-				p:SetLifeTime(1)
+					if vm then
+						pos = vector_origin
+					else
+						if not pos or pos == ent:GetPos() then continue end
+					end
 
-				p:SetStartSize(math.Rand(2,4))
-				p:SetEndSize(0)
-
-				p:SetStartAlpha(0)
-				p:SetEndAlpha(255)
-
-				p:SetColor(color.r, color.g, color.b)
-
-				p:SetVelocity(VectorRand()*5)
-				p:SetGravity(Vector(0,0,3))
-				p:SetAirResistance(30)
-
-				ent.jrpg_items_next_emit2 = time + 0.1 + (vm and 0.4 or 0)
-
-				local intensity = color:Length()/100
-
-				if math_random() > 0.2 then
 					local p = emitter2d:Add(glare2_mat, pos + (VectorRand()*radius*0.5))
-					p:SetDieTime(math.Rand(1,3))
+					p:SetDieTime(math.Rand(2,4))
 					p:SetLifeTime(1)
 
 					p:SetStartSize(math.Rand(2,4))
 					p:SetEndSize(0)
 
-					p:SetStartAlpha(255)
+					p:SetStartAlpha(0)
 					p:SetEndAlpha(255)
 
-					p:SetVelocity(VectorRand()*3)
-					p:SetGravity(Vector(0,0,math.Rand(3,5)))
+					p:SetColor(color.r, color.g, color.b)
+
+					p:SetVelocity(VectorRand()*5)
+					p:SetGravity(Vector(0,0,3))
 					p:SetAirResistance(30)
 
-					p:SetNextThink(CurTime())
+					local intensity = color:Length()/100
 
-					local seed = math_random()
-					local seed2 = math.Rand(-4,4)
+					if math_random() > 0.2 then
+						local p = emitter2d:Add(glare2_mat, pos + (VectorRand()*radius*0.5))
+						p:SetDieTime(math.Rand(1,3))
+						p:SetLifeTime(1)
 
-					p:SetThinkFunction(function(p)
-						p:SetStartSize(math_abs(math_sin(seed+time*seed2)*3*intensity+math.Rand(0,2)))
-						p:SetColor(math.Rand(200/intensity, 255), math.Rand(200/intensity, 255), math.Rand(200/intensity, 255))
+						p:SetStartSize(math.Rand(2,4))
+						p:SetEndSize(0)
+
+						p:SetStartAlpha(255)
+						p:SetEndAlpha(255)
+
+						p:SetVelocity(VectorRand()*3)
+						p:SetGravity(Vector(0,0,math.Rand(3,5)))
+						p:SetAirResistance(30)
+
 						p:SetNextThink(CurTime())
-					end)
+
+						local seed = math_random()
+						local seed2 = math.Rand(-4,4)
+
+						p:SetThinkFunction(function(p)
+							p:SetStartSize(math_abs(math_sin(seed+time*seed2)*3*intensity+math.Rand(0,2)))
+							p:SetColor(math.Rand(200/intensity, 255), math.Rand(200/intensity, 255), math.Rand(200/intensity, 255))
+							p:SetNextThink(CurTime())
+						end)
+
+						if vm then break end
+					end
+					emitted = emitted + 1
 				end
+				ent.jrpg_items_next_emit2 = time + (0.1 * emitted) + (vm and 0.4 or 0)
 			end
 
 			if not ent.jrpg_items_next_emit or ent.jrpg_items_next_emit < time then
-				local p = emitter2d:Add(math_random() > 0.5 and smoke_mat or smoke2_mat, pos)
-				p:SetDieTime(3)
-				p:SetLifeTime(1)
+				local emitted = 1
+				for _, atch in ipairs(ent:GetAttachments()) do
+					local pos = ent:GetAttachment(atch.id).Pos
+					if vm then
+						pos = vector_origin
+					else
+						if not pos or pos == ent:GetPos() then continue end
+					end
 
-				p:SetStartSize(1)
-				p:SetEndSize(15 * (vm and 0.4 or 1))
+					local p = emitter2d:Add(math_random() > 0.5 and smoke_mat or smoke2_mat, pos)
+					p:SetDieTime(3)
+					p:SetLifeTime(1)
 
-				p:SetStartAlpha(255*vis)
-				p:SetEndAlpha(0)
+					p:SetStartSize(1)
+					p:SetEndSize(15 * (vm and 0.4 or 1))
 
-				p:SetColor(color.r, color.g, color.b)
+					p:SetStartAlpha(255*vis)
+					p:SetEndAlpha(0)
 
-				p:SetVelocity(VectorRand()*3)
+					p:SetColor(color.r, color.g, color.b)
 
-				p:SetRoll(math_random()*360)
+					p:SetVelocity(VectorRand()*3)
 
-				p:SetAirResistance(30)
-				ent.jrpg_items_next_emit = time + 0.2 + (vm and 0.4 or 0)
+					p:SetRoll(math_random()*360)
+
+					p:SetAirResistance(30)
+					emitted = emitted + 1
+
+					if vm then break end
+				end
+				ent.jrpg_items_next_emit = time + (0.2 * emitted) + (vm and 0.4 or 0)
 			end
 		end
 
-		if vm then return end
+		if vm or wm then return end
 
 		render_SetMaterial(fire_mat)
 
@@ -393,7 +424,6 @@ if CLIENT then
 
 	hook.Add("RenderScreenspaceEffects", "jrpg_items", function()
 		render.UpdateScreenEffectTexture()
-		render.ModelMaterialOverride(shiny)
 		local time = RealTime()
 
 		cam.Start3D()
@@ -404,19 +434,17 @@ if CLIENT then
 			end
 
 			local color = get_color(ent)
-			render_SetColorModulation(color.r/255 , color.g/255, color.b/255)
+
+			local wm = false
 
 			if ent:GetMoveType() ~= MOVETYPE_VPHYSICS then
 				local ply = ent:GetOwner()
 				if ply:IsValid() then
-					if ply:GetActiveWeapon() == ent and ent:GetAttachment(1) and (ply ~= LocalPlayer() or ply:ShouldDrawLocalPlayer()) then
-						ent:GetOwner().item_pickup_player_draw_hack = true
-						draw_glow(ent, RealTime(), ent:GetAttachment(1).Pos, 0, 10, 1, color, true)
-						ent:GetOwner().item_pickup_player_draw_hack = nil
+					if ply:GetActiveWeapon() ~= ent or (ply == LocalPlayer() and not ply:ShouldDrawLocalPlayer()) then
+						continue
 					end
 				end
-
-				continue
+				wm = true
 			end
 
 			local pos = ent:WorldSpaceCenter()
@@ -430,8 +458,10 @@ if CLIENT then
 			if vis == 0 and util_PixelVisible(pos, radius*5, ent.jrpg_items_pixvis2) == 0 then continue end
 
 			local distance = pos:Distance(EyePos())
-
-			draw_glow(ent, time, pos, distance, radius, vis, color)
+			render.ModelMaterialOverride()
+			render_SetColorModulation(1,1,1)
+			ent:DrawModel()
+			draw_glow(ent, time, distance, wm and 5 or radius, vis * (wm and 0.25 or 1), color, nil, wm)
 		end
 
 		render.SetColorModulation(1,1,1)
@@ -454,38 +484,35 @@ if CLIENT then
 		suppress = false
 		local time = RealTime()
 
-		for i, info in ipairs(ent:GetAttachments()) do
-			local posang = ent:GetAttachment(info.id)
+		local color = get_color(wep)
+		shiny:SetVector("$color2", Vector(color.r/255,color.g/255,color.b/255) * 0.5)
 
-			if posang then
-				local color = get_color(wep)
-				shiny:SetVector("$color2", Vector(color.r/255,color.g/255,color.b/255) * 0.5)
+		shiny:SetFloat("$RimlightBoost", 1)
 
-				shiny:SetFloat("$RimlightBoost", 1)
+		local old = emitter2d
+		emitter2d = emitter_viewmodel
 
+		suppress = true
+		draw_glow(ent, time, 0, 10, 0.2, color, true)
+		suppress = false
 
-				suppress = true
-				render.ModelMaterialOverride(shiny)
-
-				local old = emitter2d
-				emitter2d = emitter_viewmodel
-
-				draw_glow(ent, time, vector_origin, 0, 10, 0.2, color, true)
-
+		for i, atch in ipairs(ent:GetAttachments()) do
+			local atch = ent:GetAttachment(atch.id)
+			local pos, ang = atch.Pos, atch.Ang
+			if pos then
 				ent.item_pickup_vm_rand_ang = ent.item_pickup_vm_rand_ang or {}
 				ent.item_pickup_vm_rand_ang[i] = ent.item_pickup_vm_rand_ang[i] or VectorRand():Angle()
 
-				cam.Start3D(WorldToLocal(EyePos(), EyeAngles(), posang.Pos, posang.Ang + ent.item_pickup_vm_rand_ang[i]))
+				cam.Start3D(WorldToLocal(EyePos(), EyeAngles(), pos, ang + ent.item_pickup_vm_rand_ang[i]))
 				emitter_viewmodel:Draw()
 				cam.End3D()
-
-				emitter2d = old
-
-				render.ModelMaterialOverride()
-				suppress = false
 			end
 		end
 
+		emitter2d = old
+
+		render.ModelMaterialOverride()
+		suppress = false
 		shiny:SetFloat("$RimlightBoost", 10)
 
 		return true
