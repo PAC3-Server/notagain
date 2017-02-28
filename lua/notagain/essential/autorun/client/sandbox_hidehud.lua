b78b193ce2312667dc18a6e67026b42cdcded256
CapsAdmin <eliashogstvedt@gmail.com>
2017-02-28 10:25:10 +0100

    hide ammo hud if current ammo is max

--- lua/notagain/essential/autorun/client/sandbox_hidehud.lua
+++ lua/notagain/essential/autorun/client/sandbox_hidehud.lua
@@ -1,7 +1,22 @@
 hook.Add("HUDShouldDraw", "hide_hud", function(element)
 	local ply = LocalPlayer()
 	if not ply:IsValid() then return end
+	
 	if (element == "CHudHealth" and ply:Health() == ply:GetMaxHealth()) or (element == "CHudBattery" and ply:Armor() == 100) then
 		return false
+	end
+	
+	if element == "CHudAmmo" or element == "CHudSecondaryAmmo" then
+		local wep = ply:GetActiveWeapon()
+
+		if wep:IsValid() then
+			if element == "CHudAmmo" and wep:Clip1() == wep:GetMaxClip1() then
+				return false
+			end
+			
+			if element == "CHudSecondaryAmmo" and wep:Clip2() == wep:GetMaxClip2() then
+				return false
+			end
+		end
 	end
 end)
