b0a194c2e3b92b4c4d478e32f857e72cc798dc1a
CapsAdmin <eliashogstvedt@gmail.com>
2017-03-06 10:46:39 +0100

    forgot to update aowl command

--- lua/notagain/jrpg/aowl_commands/avatar.lua
+++ lua/notagain/jrpg/aowl_commands/avatar.lua
@@ -1,18 +1,16 @@
-aowl.AddCommand("avatar", function(ply, _, url,w,h,cx,cy,s)
+aowl.AddCommand("avatar", function(ply, _, url,cx,cy,s)
 	if not url then
 		avatar.SetPlayer(ply)
 		return
 	end
 
-	if not w then
-		return false, "usage: !avatar url, width, height, center_x, center_y, scale"
+	if not cx then
+		return false, "usage: !avatar url, center_x, center_y, scale"
 	end
 
-	w = tonumber(w)
-	h = tonumber(h) or w
-	cx = tonumber(cx) or w/2
-	cy = tonumber(cy) or h/2
+	cx = tonumber(cx) or 0
+	cy = tonumber(cy) or 0
 	s = tonumber(s) or 1
 
-	avatar.SetPlayer(ply, url,w,h,cx,cy,s)
+	avatar.SetPlayer(ply, url,cx,cy,s)
 end)
