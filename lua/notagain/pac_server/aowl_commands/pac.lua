7308354f818cc870f36dbdd5e4a70adf505c4053
Yara // Réno <reno.reboursier@gmail.com>
2017-03-12 13:30:43 +0100

    added !ignorepac and !unignorepac

--- lua/notagain/pac_server/aowl_commands/pac.lua
+++ lua/notagain/pac_server/aowl_commands/pac.lua
@@ -1,3 +1,5 @@
+local easylua = requirex("easylua")
+
 aowl.AddCommand("fixpac", function(ply)
 	if IsValid(ply) and pace then
 		ply.pac_requested_outfits = false
@@ -9,4 +11,26 @@ aowl.AddCommand("wear", function(ply,line,file)
 	if IsValid(ply) and file then
 		ply:ConCommand("pac_wear_parts \"" .. file.."\"")
 	end
+end)
+
+aowl.AddCommand("ignorepac",function(ply,line,target)
+	target = easylua.FindEntity(target)
+
+	if target and IsValid(target) and Isvalid(ply) and target:IsPlayer() then
+
+		ply:SendLua([[pac.IgnoreEntity(]]..target..[[)]])
+
+	end
+
+end)
+
+aowl.AddCommand("unignorepac",function(ply,line,target)
+	target = easylua.FindEntity(target)
+
+	if target and IsValid(target) and IsValid(ply) and target:IsPlayer() then
+
+		ply:SendLua([[pac.UnIgnoreEntity(]]..target..[[)]])
+
+	end
+
 end)
