5dc072168b71a3b61e8ea1c3ef322bdf4f15ab62
CapsAdmin <eliashogstvedt@gmail.com>
2017-03-15 15:16:27 +0100

    improve chat print info

--- lua/notagain/pac_server/autorun/server/mediaplayer_listener_chatprint.lua
+++ lua/notagain/pac_server/autorun/server/mediaplayer_listener_chatprint.lua
@@ -1,7 +1,9 @@
 hook.Add("MediaPlayerAddListener", "mediaplayer_listener_chatprint", function(mp, ply)
 	for k, v in pairs(mp:GetListeners()) do
-		v:ChatPrint(ply:Nick() .. " has subscribed to " .. tostring(mp))
-		ply:ChatPrint(ply:Nick() .. " has subscribed to " .. tostring(mp))
+		if ply ~= v then
+			v:ChatPrint(ply:Nick() .. " has subscribed to " .. tostring(mp))
+			ply:ChatPrint(v:Nick() .. " is subscribed to " .. tostring(mp))
+		end
 	end
 end)
 
