eeb043704f2a1cddd0603562d79a5ea4e43f619e
CapsAdmin <eliashogstvedt@gmail.com>
2017-03-17 04:19:20 +0100

    fix error

--- lua/notagain/pac_server/autorun/server/mediaplayer_listener_chatprint.lua
+++ lua/notagain/pac_server/autorun/server/mediaplayer_listener_chatprint.lua
@@ -1,6 +1,6 @@
 hook.Add("MediaPlayerAddListener", "mediaplayer_listener_chatprint", function(mp, ply)
 	for k, v in pairs(mp:GetListeners()) do
-		if ply ~= v then
+		if ply ~= v and v:IsValid() then
 			v:ChatPrint(ply:Nick() .. " has subscribed to " .. tostring(mp))
 			ply:ChatPrint(v:Nick() .. " is subscribed to " .. tostring(mp))
 		end
@@ -9,6 +9,8 @@ end)
 
 hook.Add("MediaPlayerRemoveListener", "mediaplayer_listener_chatprint", function(mp, ply)
 	for k, v in pairs(mp:GetListeners()) do
-		v:ChatPrint(ply:Nick() .. " has unsubscribed from " .. tostring(mp))
+		if v:IsValid() then
+			v:ChatPrint(ply:Nick() .. " has unsubscribed from " .. tostring(mp))
+		end
 	end
 end)
