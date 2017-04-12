b589f5a7506579b2f22a730e225e03d982db8c15
CapsAdmin <eliashogstvedt@gmail.com>
2017-04-13 00:24:46 +0200

    improved blur outline gradient support shadow support

--- lua/notagain/jrpg/libraries/client/pretty_text.lua
+++ lua/notagain/jrpg/libraries/client/pretty_text.lua
@@ -10,7 +10,7 @@ local function create_fonts(font, size, weight, blursize)
 			font = font,
 			size = size,
 			weight = weight,
-			antialias 	= true,
+			antialias = true,
 		}
 	)
 
@@ -20,8 +20,9 @@ local function create_fonts(font, size, weight, blursize)
 			font = font,
 			size = size,
 			weight = weight,
-			antialias 	= true,
-			blursize = blursize,
+			antialias= true,
+			blursize = blursize/2,
+			outline = true,
 		}
 	)
 
@@ -35,24 +36,62 @@ end
 local def_color1 = Color(255, 255, 255, 255)
 local def_color2 = Color(0, 0, 0, 255)
 
+local STENCILOPERATION_KEEP = STENCILOPERATION_KEEP
+local STENCILOPERATION_REPLACE = STENCILOPERATION_REPLACE
+local STENCILCOMPARISONFUNCTION_ALWAYS = STENCILCOMPARISONFUNCTION_ALWAYS
+local STENCILCOMPARISONFUNCTION_EQUAL = STENCILCOMPARISONFUNCTION_EQUAL
+
 local surface_SetFont = surface.SetFont
 local surface_SetTextColor = surface.SetTextColor
 local surface_SetTextPos = surface.SetTextPos
 local surface_DrawText = surface.DrawText
 local surface_GetTextSize = surface.GetTextSize
+
+local render_PushRenderTarget = render.PushRenderTarget
+local render_OverrideAlphaWriteEnable = render.OverrideAlphaWriteEnable
+local render_Clear = render.Clear
+local render_PopRenderTarget = render.PopRenderTarget
+local surface_DisableClipping = surface.DisableClipping
+local render_ClearStencil = render.ClearStencil
+local render_SetStencilEnable = render.SetStencilEnable
+local render_SetStencilWriteMask = render.SetStencilWriteMask
+local render_SetStencilTestMask = render.SetStencilTestMask
+local render_SetStencilReferenceValue = render.SetStencilReferenceValue
+local render_SetStencilFailOperation = render.SetStencilFailOperation
+local render_SetStencilZFailOperation = render.SetStencilZFailOperation
+local render_SetStencilPassOperation = render.SetStencilPassOperation
+local render_SetStencilCompareFunction = render.SetStencilCompareFunction
+local render_SetMaterial = render.SetMaterial
+local render_DrawScreenQuad = render.DrawScreenQuad
+local surface_SetDrawColor = surface.SetDrawColor
+local surface_SetMaterial = surface.SetMaterial
+local surface_DrawTexturedRect = surface.DrawTexturedRect
+local ColorToHSV = ColorToHSV
+local HSVToColor = HSVToColor
 local Color = Color
 
 local hsv_cache = {}
 
 local prettytext = {}
 
-function prettytext.Draw(text, x, y, font, size, weight, blursize, color1, color2, x_align, y_align, shadow_overdraw)
+local stencil_tex = GetRenderTarget("pretty_text_stencil_" .. os.clock(), ScrW(),ScrH(), false)
+local stencil_mat = CreateMaterial("pretty_text_stencil_mat" .. os.clock(), "UnlitGeneric", {
+	["$alphatest"] = "1",
+	["$alphatestreference"] = "0.01",
+	["$translucent"] = "1",
+	["$vertexcolor"] = "1",
+	["$vertexalpha"] = "1",
+})
+
+stencil_mat:SetTexture("$basetexture", stencil_tex)
+
+function prettytext.Draw(text, x, y, font, size, weight, blursize, color1, color2, x_align, y_align, blur_overdraw, shadow, gradient_mat, gr,gg,gb,ga)
 	font = font or "arial"
 	size = size or 14
 	weight = weight or 0
 	blursize = blursize or 1
 	color1 = color1 or def_color1
-	shadow_overdraw = shadow_overdraw or 5
+	blur_overdraw = blur_overdraw or 5
 
 	if color2 == true then
 		hsv_cache[color1.r] = hsv_cache[color1.r] or {}
@@ -81,8 +120,7 @@ function prettytext.Draw(text, x, y, font, size, weight, blursize, color1, color
 	if not fonts[font] then fonts[font] = {} end
 	if not fonts[font][size] then fonts[font][size] = {} end
 	if not fonts[font][size][weight] then fonts[font][size][weight] = {} end
-	if not fonts[font][size][weight][blursize] then fonts[font][size][weight][blursize] = {} end
-	if not fonts[font][size][weight][blursize][shadow_overdraw] then fonts[font][size][weight][blursize][shadow_overdraw] = create_fonts(font, size, weight, blursize) end
+	if not fonts[font][size][weight][blursize] then fonts[font][size][weight][blursize] = create_fonts(font, size, weight, blursize) end
 
 	local w, h = prettytext.GetTextSize(text, font, size, weight, blursize)
 	if x_align then
@@ -93,36 +131,85 @@ function prettytext.Draw(text, x, y, font, size, weight, blursize, color1, color
 		y = y + (h * y_align)
 	end
 
-	surface_SetFont(fonts[font][size][weight][blursize][shadow_overdraw].blur)
+	surface_SetFont(fonts[font][size][weight][blursize].blur)
+
+	if shadow then
+		surface_SetTextColor(0,0,0,255)
+		for _ = 1, 15 do
+			surface_SetTextPos(x+shadow, y+shadow)
+			surface_DrawText(text)
+		end
+	end
+
 	surface_SetTextColor(Color(color2.r, color2.g, color2.b, color2.a * (color1.a/255)))
 
-	for i = 1, shadow_overdraw do
-		surface_SetTextPos(x, y) -- this resets for some reason after drawing
+	for _ = 1, blur_overdraw do
+		surface_SetTextPos(x, y)
 		surface_DrawText(text)
 	end
 
-	surface_SetFont(fonts[font][size][weight][blursize][shadow_overdraw].main)
+	if gradient_mat then
+		render_PushRenderTarget(stencil_tex)
+		render_OverrideAlphaWriteEnable( true, true )
+		render_Clear(0, 0, 0, 0)
+		surface_SetTextColor(255, 255, 255, 255)
+		surface_SetFont(fonts[font][size][weight][blursize].main)
+		surface_SetTextPos(x, y)
+		surface_DrawText(text)
+		render_OverrideAlphaWriteEnable(false)
+		render_PopRenderTarget()
+
+		surface_DisableClipping(true)
+		render_ClearStencil()
+		render_SetStencilEnable(true)
+		render_SetStencilWriteMask(255)
+		render_SetStencilTestMask(255)
+		render_SetStencilReferenceValue(15)
+		render_SetStencilFailOperation(STENCILOPERATION_KEEP)
+		render_SetStencilZFailOperation(STENCILOPERATION_KEEP)
+		render_SetStencilPassOperation(STENCILOPERATION_REPLACE)
+		render_SetStencilCompareFunction(STENCILCOMPARISONFUNCTION_ALWAYS)
+			render_SetMaterial(stencil_mat)
+			render_DrawScreenQuad()
+		render_SetStencilCompareFunction(STENCILCOMPARISONFUNCTION_EQUAL)
+	end
+
+	-- draw text1
+	surface_SetFont(fonts[font][size][weight][blursize].main)
 	surface_SetTextColor(color1)
 	surface_SetTextPos(x, y)
 	surface_DrawText(text)
 
+	if gradient_mat then
+		gr = gr or 255
+		gg = gg or 255
+		gb = gb or 255
+		ga = ga or 255
+
+		-- draw gradient
+		surface_SetDrawColor(gr,gg,gb,ga)
+		surface_SetMaterial(gradient_mat)
+		surface_DrawTexturedRect(x,y,w,h)
+
+		render_SetStencilEnable(false)
+		surface_DisableClipping(false)
+	end
+
 	return w, h
 end
 
-function prettytext.GetTextSize(text, font, size, weight, blursize, shadow_overdraw)
+function prettytext.GetTextSize(text, font, size, weight, blursize)
 	font = font or "arial"
 	size = size or 14
 	weight = weight or 0
 	blursize = blursize or 1
-	shadow_overdraw = shadow_overdraw or 5
 
 	if not fonts[font] then fonts[font] = {} end
 	if not fonts[font][size] then fonts[font][size] = {} end
 	if not fonts[font][size][weight] then fonts[font][size][weight] = {} end
-	if not fonts[font][size][weight][blursize] then fonts[font][size][weight][blursize] = {} end
-	if not fonts[font][size][weight][blursize][shadow_overdraw] then fonts[font][size][weight][blursize][shadow_overdraw] = create_fonts(font, size, weight, blursize) end
+	if not fonts[font][size][weight][blursize] then fonts[font][size][weight][blursize] = create_fonts(font, size, weight, blursize) end
 
-	surface_SetFont(fonts[font][size][weight][blursize][shadow_overdraw].main)
+	surface_SetFont(fonts[font][size][weight][blursize].main)
 	return surface_GetTextSize(text)
 end
 
