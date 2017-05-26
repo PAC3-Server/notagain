d4e993718d9a333e5f5b3f4cec257275f3696f1a
R.Réno <reno.reboursier@gmail.com>
2017-05-26 19:41:07 +0200

    file management && sync client

--- lua/notagain/pac_server/autorun/MapDefine.lua
+++ lua/notagain/pac_server/autorun/MapDefine.lua
@@ -1,81 +1,7 @@
-if not SERVER then return end
-
 local MapDefine = {}
 _G.MapDefine = MapDefine
 
 MapDefine.Areas = {}
-
-MapDefine.CreateArea = function(name,minvec,maxvec)
-	local x1,y1,z1 = minvec.x,minvec.y,minvec.z 
-	local x2,y2,z2 = maxvec.x,maxvec.y,maxvec.z 
-	local refs = {}
-
-	-- x's --
-	do
-		local bigger,smaller 
-		if x1 > x2 then
-			bigger  = x1 
-			smaller = x2
-		else
-			bigger  = x2 
-			smaller = x1
-		end
-
-		refs.XMax = bigger 
-		refs.XMin = smaller
-
-	end
-
-	-- y's -- 
-	do
-		local bigger,smaller 
-		if y1 > y2 then
-			bigger  = y1
-			smaller = y2
-		else
-			bigger  = y2
-			smaller = y1
-		end
-
-		refs.YMax = bigger 
-		refs.YMin = smaller
-	end
-
-	-- z's --
-	do
-		local bigger,smaller 
-		if z1 > z2 then
-			bigger  = z1
-			smaller = z2
-		else
-			bigger  = z2
-			smaller = z1
-		end
-
-		refs.ZMax = bigger 
-		refs.ZMin = smaller
-	end
-
-	local points = {
-		
-		["FrontBottomRight"] = Vector(refs.XMin,refs.YMin,refs.ZMin),
-		["FrontBottomLeft"]  = Vector(refs.XMax,refs.YMin,refs.ZMin),
-		["BackBottomRight"]  = Vector(refs.XMax,refs.YMax,refs.ZMin),
-		["BackBottomLeft"]   = Vector(refs.XMin,refs.YMax,refs.ZMin),
-		["FrontTopRight"]    = Vector(refs.XMin,refs.YMin,refs.ZMax),
-		["FrontTopLeft"]     = Vector(refs.XMax,refs.YMin,refs.ZMax),
-		["BackTopRight"]     = Vector(refs.XMax,refs.YMax,refs.ZMax),
-		["BackTopLeft"]      = Vector(refs.XMin,refs.YMax,refs.ZMax),
-	
-	}
-
-	MapDefine.Areas[name] = {}
-	MapDefine.Areas[name].Points = points
-	MapDefine.Areas[name].Refs   = refs
-	MapDefine.Areas[name].Map    = game.GetMap()
-
-	return refs,points
-end
 
 MapDefine.IsExistingArea = function(area)
 	return MapDefine.Areas[area] and true or false
@@ -102,33 +28,160 @@ MapDefine.IsInArea = function(area,ent)
 end
 
 MapDefine.GetCurrentArea = function(ent)
+	local areas = {}
 	for area,_ in pairs(MapDefine.Areas) do
-		if MapDefine.IsInArea(tostring(area),ent) then 
-			return area
+		local a = tostring(area)
+		if MapDefine.IsInArea(a,ent) then 
+			table.insert(areas,a)
 		end
 	end
-	return "none"
+	return areas
 end
 
-MapDefine.SaveArea = function(area)
-	if not MapDefine.IsExistingArea(area) then return end
-	tbl = MapDefine.Areas[area]
-	tbl.Name = area
-	local json = util.TableToJSON( tbl ) 
-	file.CreateDir( "mapsavedareas" ) 
-	file.CreateDir( "mapsavedareas/"..tbl.Map)
-	file.Write( "mapsavedareas/"..tbl.Map.."/"..area..".txt", json ) 
-end
+if SERVER then 
 
-MapDefine.LoadAreas = function()
-	local path = "mapsavedareas/"..game.GetMap().."/"
-	for _,file_name in ipairs((file.Find(path.."*.txt","DATA"))) do
-		local tbl = util.JSONToTable(file.Read(path..file_name,"DATA"))
-		MapDefine.Areas[tbl.Name] = tbl
-		MapDefine.Areas[tbl.Name].Name = nil
+	util.AddNetworkString("MapSyncAreas")
+	
+	MapDefine.CreateArea = function(name,minvec,maxvec)
+		local x1,y1,z1 = minvec.x,minvec.y,minvec.z 
+		local x2,y2,z2 = maxvec.x,maxvec.y,maxvec.z 
+		local refs = {}
+
+		-- x's --
+		do
+			local bigger,smaller 
+			if x1 > x2 then
+				bigger  = x1 
+				smaller = x2
+			else
+				bigger  = x2 
+				smaller = x1
+			end
+
+			refs.XMax = bigger 
+			refs.XMin = smaller
+
+		end
+
+		-- y's -- 
+		do
+			local bigger,smaller 
+			if y1 > y2 then
+				bigger  = y1
+				smaller = y2
+			else
+				bigger  = y2
+				smaller = y1
+			end
+
+			refs.YMax = bigger 
+			refs.YMin = smaller
+		end
+
+		-- z's --
+		do
+			local bigger,smaller 
+			if z1 > z2 then
+				bigger  = z1
+				smaller = z2
+			else
+				bigger  = z2
+				smaller = z1
+			end
+
+			refs.ZMax = bigger 
+			refs.ZMin = smaller
+		end
+
+		local points = {
+			
+			["1"] = Vector(refs.XMin,refs.YMin,refs.ZMin),
+			["2"] = Vector(refs.XMax,refs.YMin,refs.ZMin),
+			["3"] = Vector(refs.XMax,refs.YMax,refs.ZMin),
+			["4"] = Vector(refs.XMin,refs.YMax,refs.ZMin),
+			["5"] = Vector(refs.XMin,refs.YMin,refs.ZMax),
+			["6"] = Vector(refs.XMax,refs.YMin,refs.ZMax),
+			["7"] = Vector(refs.XMax,refs.YMax,refs.ZMax),
+			["8"] = Vector(refs.XMin,refs.YMax,refs.ZMax),
+		
+		}
+
+		MapDefine.Areas[name] = {}
+		MapDefine.Areas[name].Points = points
+		MapDefine.Areas[name].Refs   = refs
+		MapDefine.Areas[name].Map    = game.GetMap()
+
+		net.Start("MapSyncAreas")
+		net.WriteTable(MapDefine.Areas)
+		net.Broadcast()
+
+		return refs,points
 	end
+
+	MapDefine.SaveArea = function(area)
+		if not MapDefine.IsExistingArea(area) then return end
+		tbl = MapDefine.Areas[area]
+		tbl.Name = area
+		local json = util.TableToJSON( tbl ) 
+		file.CreateDir( "mapsavedareas" ) 
+		file.CreateDir( "mapsavedareas/"..tbl.Map)
+		file.Write( "mapsavedareas/"..tbl.Map.."/"..area..".txt", json ) 
+	end
+
+	MapDefine.DeleteArea = function(area,map)
+		local map = map or game.GetMap()
+		file.Delete("mapsavedareas/"..map.."/"..area..".txt")
+	end
+
+	MapDefine.SaveAll = function()
+		for area,_ in pairs(MapDefine.Areas) do
+			MapDefine.SaveArea(tostring(area))
+		end
+	end
+
+	MapDefine.DeleteAll = function(all)
+		if all then 
+			for _,map in pairs(select(2,file.Find("mapsavedareas/*","DATA"))) do
+				for _,area in pairs((file.Find("mapsavedareas/"..map.."/*.txt","DATA"))) do
+					file.Delete("mapsavedareas/"..map.."/"..area)
+				end
+				file.Delete("mapsavedareas/"..map)
+			end
+		else 
+			for area,_ in pairs(MapDefine.Areas) do
+				MapDefine.DeleteArea(tostring(area))
+			end
+		end
+	end
+
+	MapDefine.LoadAreas = function()
+		local path = "mapsavedareas/"..game.GetMap().."/"
+		local areas = {}
+		for _,file_name in ipairs((file.Find(path.."*.txt","DATA"))) do
+			local tbl = util.JSONToTable(file.Read(path..file_name,"DATA"))
+			areas[tbl.Name] = tbl
+			areas[tbl.Name].Name = nil
+		end
+		
+		MapDefine.Areas = areas
+		
+		net.Start("MapSyncAreas")
+		net.WriteTable(areas)
+		net.Broadcast()
+	
+	end
+
+	hook.Add("Initialize","LoadMapDefineAreas",MapDefine.LoadAreas)
+
 end
 
-hook.Add("Initialize","LoadMapDefineAreas",MapDefine.LoadAreas)
+if CLIENT then
+
+	net.Receive("MapSyncAreas",function()
+		local tbl = net.ReadTable()
+		MapDefine.Areas = tbl
+	end)
+
+end
 
 return MapDefine
