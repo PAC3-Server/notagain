c5151a302a01da2c0f592637118d021e9e1c6daa
R.Réno <reno.reboursier@gmail.com>
2017-05-30 16:32:27 +0200

    Attempt to register all timers properly

--- lua/notagain/debug/autorun/timers.lua
+++ lua/notagain/debug/autorun/timers.lua
@@ -1,6 +1,9 @@
 local Timers = Timers or {}
 
-local old_timercreate = timer.Create 
+local old_timercreate  = timer.Create 
+local old_timeradjust  = timer.Adjust
+local old_timerremove  = timer.Remove 
+local old_timerdestroy = timer.Destroy
 
 timer.Create = function(name,delay,rep,callback)
 	Timers[name] = {
@@ -8,7 +11,47 @@ timer.Create = function(name,delay,rep,callback)
 		Repetitions = rep == 0 and "inf" or rep,
 		Callback = callback,
 	}
+	if Timers[name].Repetitions ~= "inf" then
+		
+		local removal = function(time)
+			if timer.Exists(name) then
+				timer.Simple(time,function() removal(time) end)
+			else
+				timers[name] = nil 
+			end
+		end
+		
+		timer.Simple(Timers[name].Repetitions,function() removal(Timers[name].Repetitions) end)
+	end
 	old_timercreate(name,delay,rep,callback)
+end
+
+timer.Adjust = function(name,delay,rep,callback)
+	if Timers[name] then
+		Timers[name] = {
+			Delay = delay,
+			Repetitions = rep == 0 and "inf" or rep, 
+			Callback = callback,
+		}
+		old_timeradjust(name,delay,rep,callback)
+		return true 
+	else
+		return false 
+	end
+end
+
+timer.Remove = function(name)
+	if Timers[name] then
+		Timers[name] = nil 
+	end
+	old_timerremove(name)
+end
+
+timer.Destroy = function(name)
+	if Timers[name] then
+		Timers[name] = nil 
+	end
+	old_timerdestroy(name)
 end
 
 timer.GetAll = function()
