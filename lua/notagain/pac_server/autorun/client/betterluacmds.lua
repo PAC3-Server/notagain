45b75d8836baa4b69bbdff6089a436aabdf15786
R.Réno <reno.reboursier@gmail.com>
2017-06-01 15:36:23 +0200

    Better display for lua exec in chat

---
+++ lua/notagain/pac_server/autorun/client/betterluacmds.lua
@@ -0,0 +1,62 @@
+----lua chat beautify---
+local LuaChat = {}
+
+LuaChat.Cmds = {
+	["l"]      = "Server",
+	["lm"]     = "Self",
+	["ls"]     = "Shared",
+	["lb"]     = "Both",
+	["lc"]     = "Clients",
+	["print"]  = "Server Print",
+	["table"]  = "Server Table",
+	["keys"]   = "Server Keys",
+	["printm"] = "Self Print",
+	["printb"] = "Both Print",
+}
+
+LuaChat.OnClientCmds = {
+	["lsc"]    = "lsc",
+	["printc"] = "printc",
+}
+
+LuaChat.IsCommand = function(str)
+	local s = string.lower(str)
+	local _,replaced = string.gsub(s,"^[!|%.|/]","")
+
+	return replaced >= 1 and true or false
+end
+
+LuaChat.DoLuaCommand = function(ply,str)
+
+	if LuaChat.IsCommand(str) and IsValid(ply) then
+		local str,_ = string.gsub(str,"^[!|%.|/]","")
+		local args = string.Explode(" ",str)
+		local cmd = string.lower(args[1])
+
+		if LuaChat.Cmds[cmd] then
+			
+			chat.AddText(team.GetColor(ply:Team()),ply:GetName(),Color(61,61,61)," -> ",Color(244,66,66),LuaChat.Cmds[cmd],Color(175,175,175),": "..table.concat(args," ",2,#args))
+			
+			return true
+		
+		elseif LuaChat.OnClientCmds[cmd] then
+			
+			local a = string.Explode(",",args[2])
+			
+			if LuaChat.OnClientCmds[cmd] == "lsc" then
+				chat.AddText(team.GetColor(ply:Team()),ply:GetName(),Color(61,61,61)," -> ",Color(244,66,66),a[1],Color(175,175,175),": "..a[2]..table.concat(args," ",3,#args))
+			else
+				chat.AddText(team.GetColor(ply:Team()),ply:GetName(),Color(61,61,61)," -> ",Color(244,66,66),a[1].."Print",Color(175,175,175),": "..a[2]..table.concat(args," ",3,#args))
+			end
+			
+			return true 
+		
+		end
+
+	end
+
+end
+
+hook.Add("OnPlayerChat","LuaChatCommands",LuaChat.DoLuaCommand)
+
+return LuaChat
