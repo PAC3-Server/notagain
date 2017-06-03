2f8ed98b71725158b3399599431ed8fe6d65ff5d
LuaTenshi <tenshi@mirai.red>
2017-06-02 20:15:11 -0400

    Added GMA mounter via Lua so that workshop tools can be added by everyone!

--- lua/notagain/essential/autorun/server/mount_gmas.lua
+++ lua/notagain/essential/autorun/server/mount_gmas.lua
@@ -1,8 +1,13 @@
 local path = "notagain/gmafiles/"
+local gma_path = "addons/notagain/lua/"..path
+
 local files,dirs = file.Find(path.."*", "LUA")
 for _,file in next, files do
-   print("Attempting to mount...", path..file)
-   local success, info = game.MountGMA(path..file)
-   print(path..file,success and " Has been mounted." or " !!! Mounting Failed!")
-   if success then PrintTable(info) end
+	print("Attempting to mount...", path..file)
+	local success = game.MountGMA(gma_path..file)
+	print(path..file,success and " Has been mounted." or " !!! Mounting Failed!")
+	if success then 
+		local id = string.match(file,"([0-9]+)%.gma")
+		resource.AddWorkshop(id)
+	end
 end
