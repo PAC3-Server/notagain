ee9e233282ffc7ad68ed00be1aaa82d0f16c60a7
techbot <techbot121@gmail.com>
2017-06-08 19:32:51 +0200

    this looks so bad, but it works so what ever

--- lua/notagain/pac_server/aowl_commands/discordrelay.lua
+++ lua/notagain/pac_server/aowl_commands/discordrelay.lua
@@ -1,8 +1,22 @@
 if SERVER then
     if discordrelay then
-        aowl.AddCommand({"relay","discordrelay"}, function(ply,_,com)
+        aowl.AddCommand({"relay","discordrelay"}, function(ply,_,com,com2)
             if com == "reload" then
                 discordrelay.reload()
+            elseif com == "status" then
+                local modulec = table.Count(discordrelay.modules)
+                local extensionc = table.Count(discordrelay.extensions)
+                local is_fetching = timer.Exists("DiscordRelayFetchMessages")
+                discordrelay.log("Status:","fetching messages:",is_fetching and "yes" or "no")
+                discordrelay.log("Status:",modulec,"modules and",extensionc,"extensions loaded.")
+            elseif com == "modules" or com == "extensions" then
+                if com2 == "list" then
+                    local str = ""
+                    for name,_ in pairs(discordrelay[com]) do
+                        str = str..name..", "
+                    end
+                    discordrelay.log(com..":",str)
+                end
             end
             -- todo: add more commands?
 		end)
