95cc0e78828672abab8ad04d365adb2daf0a4736
R.Réno <reno.reboursier@gmail.com>
2017-06-09 21:59:23 +0200

    store fonts, and allow you to change them in game

---
+++ lua/notagain/inserts/preload/client/fonts.lua
@@ -0,0 +1,59 @@
+local old_createfont = surface.CreateFont
+local Font = _G.Font or {}
+_G.Font = Font
+
+local all = {}
+
+surface.CreateFont = function(name,table)
+    all[name] = table
+    old_createfont(name,table)
+end
+
+Font.GetAll = function()
+    return all
+end
+
+Font.Find = function(name)
+    local results = {}
+    local name = string.lower(name)
+    for font,tbl in pairs(all) do
+        if string.match(font,name) then
+            results[font] = tbl
+        end
+    end
+    return results
+end
+
+Font.GetProperties = function(name)
+    return all[name] and all[name] or { font = "this font doesnt exist!" }
+end
+
+Font.SetProperty = function(name,field,value)
+    local font = all[name]
+    if font[field] then
+        font[field] = value
+        return true
+    else
+        return false
+    end 
+end
+
+Font.GetProperty = function(name,field)
+    if all[name] then
+        return all[name][field]
+    else
+        return nil
+    end
+end
+
+Font.Redefine = function(name,tbl)
+    if not all[name] then return false end
+
+    local oldtbl = all[name]
+
+    for field,value in pairs(tbl)
+        oldtbl[field] = value
+    end
+
+    return true
+end
