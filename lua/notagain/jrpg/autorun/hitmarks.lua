353342309616618c75bf611be0015ad270d4e825
R.Réno <reno.reboursier@gmail.com>
2017-06-09 18:09:05 +0200

    Create hitmarks.lua

--- lua/notagain/jrpg/autorun/hitmarks.lua
+++ lua/notagain/jrpg/autorun/hitmarks.lua
@@ -248,8 +248,8 @@ if CLIENT then
 				local world_pos = (ent:NearestPoint(ent:EyePos() + Vector(0,0,100000)) + Vector(0,0,2))
 				local pos = world_pos:ToScreen()
 				local dist = world_pos:Distance(EyePos())
-				local scale = (ent:GetModelScale() or 1)
-				local radius = ent:BoundingRadius() * 14
+				local scale = (ent:GetModelScale()*2 or 2)
+				local radius = ent:BoundingRadius() * 7
 				local max_distance = scale * radius
 				fraction = fraction * ((-(dist / max_distance)+1) ^ 2)
 
