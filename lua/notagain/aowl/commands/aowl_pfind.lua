acdb311524d18f33288ac1335a974611841e1d4b
R.Réno <reno.reboursier@gmail.com>
2017-06-10 01:29:43 +0200

    this will do

--- lua/notagain/aowl/commands/aowl_pfind.lua
+++ lua/notagain/aowl/commands/aowl_pfind.lua
@@ -1,11 +1,19 @@
 local pfind = "aowl_pfind"
+local bl = {
+    ["false"] = false,
+    ["true"]  = true,
+    ["yes"]   = true,
+    ["no"]    = false,
+    ["_"]     = true,
+    ["nil"]   = true,
+}
 
 if SERVER then
     util.AddNetworkString(pfind)
 
     aowl.AddCommand({"p","find"},function(ply,line,search,a,b,c,d,e,f,g)
         if search then 
-            PFind(search,a,b,c,d,e,f,g)
+            PFind(search,bl[a],bl[b],bl[c],bl[d],bl[e],bl[f],bl[g])
         end
     end)
 
@@ -13,13 +21,13 @@ if SERVER then
         if search then 
             net.Start(pfind)
             net.WriteString(search)
-            net.WriteBool(a)
-            net.WriteBool(b)
-            net.WriteBool(c)
-            net.WriteBool(d)
-            net.WriteBool(e)
-            net.WriteBool(f)
-            net.WriteBool(g)
+            net.WriteBool(bl[a] or true)
+            net.WriteBool(bl[b] or true)
+            net.WriteBool(bl[c] or true)
+            net.WriteBool(bl[d] or true)
+            net.WriteBool(bl[e] or true)
+            net.WriteBool(bl[f] or true)
+            net.WriteBool(bl[g] or true)
             net.Send(ply)
         end
     end)
