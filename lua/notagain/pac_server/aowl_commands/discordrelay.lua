3191b62fc8b30e1c27e42f8c8fcf3b1da699dda7
techbot <techbot121@gmail.com>
2017-06-13 22:00:42 +0200

    update discordrelay aowl commands

--- lua/notagain/pac_server/aowl_commands/discordrelay.lua
+++ lua/notagain/pac_server/aowl_commands/discordrelay.lua
@@ -1,6 +1,6 @@
 if SERVER then
     if discordrelay then
-        aowl.AddCommand({"relay","discordrelay"}, function(ply,_,com,com2)
+        aowl.AddCommand({"relay","discordrelay"}, function(ply,_,com,arg,mod)
             if com == "reload" then
                 discordrelay.reload()
             elseif com == "status" then
@@ -10,13 +10,20 @@ if SERVER then
                 discordrelay.log("Status:","fetching messages:",is_fetching and "yes" or "no")
                 discordrelay.log("Status:",modulec,"modules and",extensionc,"extensions loaded.")
             elseif com == "modules" or com == "extensions" then
-                if com2 == "list" then
+                if arg == "list" then
                     local str = ""
-                    for name,_ in pairs(discordrelay[com]) do
+                    for name,_ in pairs(discordrelay[com]) arg
                         str = str..name..", "
                     end
-                    discordrelay.log(com..":",str)
-                end
+                    discordrelay.log(1,"[aowl]",com..":",str)
+                elseif arg == "remove" or arg == "delete" or arg == "disable" then
+                    if not mod then return false,"No Module/Extension specified!" end
+                    if not discordrelay[com][mod] then return false,"Invalid Module/Extension" end
+                    discordrelay[com][mod].Remove()
+                elseif arg == "reload" or arg == "init" then
+                    if not mod then return false,"No Module/Extension specified!" end
+                    if not discordrelay[com][mod] then return false,"Invalid Module/Extension" end
+                    discordrelay[com][mod].Init()
             end
             -- todo: add more commands?
 		end,"developers")
