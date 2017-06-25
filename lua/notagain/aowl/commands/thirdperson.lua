9cbd29f215a6c52a8c128748a4ba5059a96e670f
R.Réno <reno.reboursier@gmail.com>
2017-06-25 14:07:47 +0200

    Update thirdperson.lua

--- lua/notagain/aowl/commands/thirdperson.lua
+++ lua/notagain/aowl/commands/thirdperson.lua
@@ -5,7 +5,7 @@ local Tag="thirdperson"
 if SERVER then
 	util.AddNetworkString(Tag)
 	
-	aowl.AddCommand({"ctp","thirdperson","view"}, function( player , line  )
+	aowl.AddCommand({"ctp","thirdperson","view","3p"}, function( player , line  )
 		if IsValid(player) then
 			net.Start(Tag)
 			net.Send(player)
