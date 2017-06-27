d663cb808b4c5f25e5906e221e521e9d96b16aab
R.Réno <reno.reboursier@gmail.com>
2017-06-27 17:18:03 +0200

    colors

--- lua/notagain/pac_server/autorun/client/friends_team.lua
+++ lua/notagain/pac_server/autorun/client/friends_team.lua
@@ -1,8 +1,8 @@
 TEAM_PLAYERS = 1
 TEAM_FRIENDS = 2
 
-team.SetUp(TEAM_PLAYERS, "players", Color(255, 70, 0, 255))
-team.SetUp(TEAM_FRIENDS, "friends", Color(60, 127, 255, 255))
+team.SetUp(TEAM_PLAYERS, "players", Color(97, 101, 117, 255))
+team.SetUp(TEAM_FRIENDS, "friends", Color(96, 178, 138, 255))
 
 local meta = FindMetaTable("Player")
 
