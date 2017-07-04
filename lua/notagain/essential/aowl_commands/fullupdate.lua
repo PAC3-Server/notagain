cea284b9e90489f9931890acdd82d026f4b7d0a8
R.Réno <reno.reboursier@gmail.com>
2017-07-04 15:19:06 +0200

    fix fullupdate

--- lua/notagain/essential/aowl_commands/fullupdate.lua
+++ lua/notagain/essential/aowl_commands/fullupdate.lua
@@ -1,6 +1,5 @@
-aowl.AddCommand({"fullupdate","update"},function(ply,line,target)
-    target = easylua.FindEntity(target)
-    if target and IsValid(target) and IsValid(ply) and target:IsPlayer() then
-      target:ConCommand("record 1;stop")
+aowl.AddCommand({"fullupdate","update"},function(ply,line)
+    if IsValid(ply) then
+      ply:ConCommand("record 1;stop")
     end
 end)
