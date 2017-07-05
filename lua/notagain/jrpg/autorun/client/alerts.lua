b6b8c2c0bcaa9a19e0f7cee8959ef4cfc1f279dc
R.Réno <reno.reboursier@gmail.com>
2017-07-05 20:42:54 +0200

    Update alerts.lua

--- lua/notagain/jrpg/autorun/client/alerts.lua
+++ lua/notagain/jrpg/autorun/client/alerts.lua
@@ -1,72 +1,73 @@
-
-AddCSLuaFile() 
-
 local scrW, scrH = ScrW(), ScrH()
 local ResolutionScale = math.Min(scrW/1600 , scrH/900)
+local JAlert = {}
+_G.JAlert = JAlert
 
-ActiveAlert = nil
-
-surface.CreateFont( "JFont", { font = "arial" , size = 18 , weight = 600 } )
+surface.CreateFont("JAlertFont",{
+    font      = "Roboto",
+    extended  = true,
+    size      = 17,
+    weight    = 500,
+    shadow    = true,
+})
 
 local ALERT = {
 	Init = function( self )
-			
 		self:SetTextColor(color_white)
-		self:SetFont("JFont")
+		self:SetFont("JAlertFont")
 		self:SetSize(256, 30)
 		self:SetContentAlignment(5)
 		self:SetExpensiveShadow(1, Color(0, 0, 0, 150))
-
 	end,
 
 	Paint = function( self , w , h )
 		local Poly = {
-	        { x = (25/ ResolutionScale),  y = h }, --100/200
-	        { x = 0,                      y = 0 }, --100/100
-	        { x = w, 					  y = 0 }, --200/100
-	        { x = w-(25/ ResolutionScale),y = h }, --200/200
+	        { x = (25/ ResolutionScale),  y = h },
+	        { x = 0,                      y = 0 },
+	        { x = w, 					  y = 0 },
+	        { x = w-(25/ ResolutionScale),y = h },
 	    }
-
 	    draw.NoTexture()
 		surface.SetDrawColor( math.abs(math.sin(CurTime()*3)*255), 0, math.abs(math.sin(CurTime()*3)*255)/4, 200)
 		surface.DrawPoly(Poly)
-
 	end,
 }
 
 vgui.Register( "DAlert" , ALERT , "DLabel" )
 
-function Alert( message , time )
-	
-	RemoveAlert() --So we don't have overlaping alerts
-	
-	local JAlert = vgui.Create( "DAlert" )
-	JAlert:SetText( message )
-	JAlert:SizeToContentsX()
-	JAlert:SetWide((JAlert:GetWide() + 64/ResolutionScale > 800) and JAlert:GetWide() + 64/ResolutionScale or 800 )
-	JAlert:SetPos( scrW/2-JAlert:GetWide()/2, -JAlert:GetTall() ) 
-	JAlert:MoveTo( JAlert.x , 0 , 0.35, 0.3)
-	JAlert.Removal = CurTime()+150 --150s max time
+JAlert.DoAlert = function(message,time)
 
-	JAlert.Think = function() -- ermergency removal
-		if JAlert.Removal <= CurTime() then
-			JAlert:MoveTo( JAlert.x , -JAlert:GetTall(), 0.35, 0.3, nil, function() JAlert:Remove() end)
-			ActiveAlert = nil
+	JAlert.RemoveAlert()
+
+	local alert = vgui.Create("DAlert")
+	alert:SetText(message)
+	alert:SizeToContentsX()
+	alert:SetWide((alert:GetWide() + 64/ResolutionScale > 800) and alert:GetWide() + 64/ResolutionScale or 800)
+	alert:SetPos(scrW/2-alert:GetWide()/2, -alert:GetTall())
+	alert:MoveTo(alert.x,0,0.35,0.3)
+
+    local time = time or 10
+    if time <= 0 then
+        time = 10
+    elseif time >= 30 then
+        time = 30
+    end
+
+    JAlert.ActiveAlert = alert
+    alert.Removal = CurTime() + time
+    alert.Think = function(self)
+		if self.Removal <= CurTime() then
+			self:MoveTo(self.x,-self:GetTall(),0.35,0.3,nil,function() self:Remove() end)
+			JAlert.ActiveAlert = nil
 		end
 	end
-	
-	ActiveAlert = JAlert
 
-	timer.Simple(time or 10,RemoveAlert)
-
-	
 end
 
-function RemoveAlert()
-	if IsValid(ActiveAlert) then
-		local temp = ActiveAlert
-		ActiveAlert = nil
-		temp:MoveTo( temp.x , -temp:GetTall(), 0.35, 0.3, nil, function() temp:Remove() end)
+JAlert.RemoveAlert = function()
+	if IsValid(JAlert.ActiveAlert) then
+		local temp = JAlert.ActiveAlert
+		JAlert.ActiveAlert = nil
+		temp:MoveTo(temp.x ,-temp:GetTall(),0.35,0.3,nil,function() temp:Remove() end)
 	end
 end
-
