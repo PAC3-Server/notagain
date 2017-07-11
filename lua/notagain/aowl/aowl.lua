d2eca97bb7e6062310a748700ffb6279e6114cd6
CapsAdmin <eliashogstvedt@gmail.com>
2017-07-11 21:26:25 +0200

    OSP

--- lua/notagain/aowl/aowl.lua
+++ lua/notagain/aowl/aowl.lua
@@ -628,6 +628,10 @@ do -- commands
 			if command.group == "localplayer" and (not CLIENT or ply ~= LocalPlayer()) then return end
 			if command.group == "clientside" and SERVER then return end
 
+			if CLIENT then
+				return
+			end
+
 			local ok = false
 			local name
 
