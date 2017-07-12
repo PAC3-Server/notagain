6c74766de59227a95bf3fce2593c7984156834ef
R.Réno <reno.reboursier@gmail.com>
2017-07-12 19:29:22 +0200

    revert ugh

--- lua/notagain/jrpg/aowl_commands/avatar.lua
+++ lua/notagain/jrpg/aowl_commands/avatar.lua
@@ -1,4 +1,4 @@
-aowl.AddCommand("avatar=nil|string,number[0],number[0],number[1]", function(ply, _, url, center_x, center_y, scale)
+aowl.AddCommand("avatar=string,number[0],number[0],number[1]", function(ply, _, url, center_x, center_y, scale)
 	if url == "reset" then
 		avatar.SetPlayer(ply)
 		return
