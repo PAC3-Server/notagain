9e5b7a823b448c2842e7eb6cfd0f1750d33ccbc8
R.Réno <reno.reboursier@gmail.com>
2017-07-13 22:16:07 +0200

    ban is not a fix to this, heres a fix

---
+++ lua/notagain/pac_server/autorun/client/hide_public_dupes.lua
@@ -0,0 +1,18 @@
+hook.Add("Initialize","hide_public_dupes",function()
+  creation_tab_old = creation_tab_old or spawnmenu.GetCreationTabs
+
+  spawnmenu.GetCreationTabs = function()
+      local HideTabs ={["#spawnmenu.category.dupes"] = true,
+                       ["#spawnmenu.category.saves"] = true}
+
+      local tabs = {}
+      for k, v in next, creation_tab_old() do
+          if not HideTabs[k] then
+              tabs[k] = v
+          end
+      end
+
+      return tabs
+  end
+  LocalPlayer():ConCommand("spawnmenu_reload")
+end)
