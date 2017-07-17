dc99e1a794278b9e0fe37c408314d1b1e38a656f
CapsAdmin <eliashogstvedt@gmail.com>
2017-07-17 10:16:00 +0200

    add custom reload rules for goluwa lua files cleanup

--- lua/notagain/goluwa/goluwa.lua
+++ lua/notagain/goluwa/goluwa.lua
@@ -1,23 +1,34 @@
 local env = {}
+_G.goluwa = env
 
-if SERVER then -- wip
+env.notagain_monitor_directories = {}
 
-	local function add(dir)
-		local files, directories = file.Find(dir .. "/*", "LUA")
+local function add(dir)
+	local files, directories = file.Find(dir .. "/*", "LUA")
 
-		for _, file_name in ipairs(files) do
-			print(dir .. "/" .. file_name, "!!!!!")
+	for _, file_name in ipairs(files) do
+		if SERVER then
 			AddCSLuaFile(dir .. "/" .. file_name)
-		end
-
-		for _, d in ipairs(directories) do
-			add(dir .. "/" .. d)
 		end
 	end
 
-	AddCSLuaFile()
-	add("notagain/goluwa/goluwa")
+	for _, dir_name in ipairs(directories) do
+		add(dir .. "/" .. dir_name)
+	end
 
+	table.insert(env.notagain_monitor_directories, {
+		dir = dir .. "/",
+		what = "clients",
+		lib = dir ~= "notagain/goluwa/goluwa" and function(code)
+			return "setfenv(1, requirex('goluwa'));" .. code
+		end,
+	})
+end
+
+AddCSLuaFile()
+add("notagain/goluwa/goluwa")
+
+if SERVER then
 	return env
 end
 
@@ -68,413 +79,7 @@ function env.desire()
 
 end
 
-do -- lua env
-	local handle_path = function(path, plain)
-		file.CreateDir("stdlua") -- hhh
-
-		path = "stdlua/" .. path
-
-		if not plain then
-			path = (path):gsub("%.", "_") .. ".txt"
-		end
-
-		return path
-	end
-
-	do -- _G
-		function env.loadstring(str, env)
-			local var = CompileString(str, env or "loadstring", false)
-			if type(var) == "string" then
-				return nil, var, 2
-			end
-			return setfenv(var, getfenv(1))
-		end
-
-		function env.loadfile(str)
-			if not file.Exists(handle_path(str, true), "DATA") then
-				return nil, str .. ": No such file", 2
-			end
-			local lua = file.Read(handle_path(str, true), "DATA")
-			return loadstring(lua, str)
-		end
-
-		function env.dofile(filename)
-			local f = assert(loadfile(filename))
-			return f()
-		end
-	end
-
-	do -- os
-		local os = {}
-
-		function os.getenv(var)
-			var = tostring(var):lower()
-
-			if var == "path" then
-				return (util.RelativePathToFull("lua/includes/init.lua"):gsub("\\", "/"):gsub("lua/includes/init.lua", ""))
-			end
-
-			if var == "username" then
-				return SERVER and "server" or LocalPlayer():Nick()
-			end
-		end
-
-		function os.setlocale(...)
-			print("os.setlocale: ", ...)
-		end
-
-		function os.execute(...)
-			print("os.execute: ", ...)
-		end
-
-		function os.exit(...)
-			print("os.exit: ", ...)
-		end
-
-		function os.remove(filename)
-			if file.Exists(handle_path(filename), "DATA") then
-				file.Delete(handle_path(filename), "DATA")
-				return true
-			end
-
-			return nil, "data/" .. filename .. ": No such file or directory", 2
-		end
-
-		function os.rename(a, b)
-			if file.Exists(handle_path(filename), "DATA") then
-				local str = file.Read(handle_path(a), "DATA")
-				file.Delete(handle_path(b), "DATA")
-				file.Write(handle_path(b), str, "DATA")
-				return true
-			end
-
-			return nil, "data/" .. a .. ": No such file or directory", 2
-		end
-
-		function os.tmpname()
-			return "data/temp/" .. util.CRC(RealTime()) .. ".txt"
-		end
-
-		os.clock = _G.os.clock
-		os.date = _G.os.date
-		os.difftime = _G.os.difftime
-		os.time = _G.os.time
-
-		env.os = os
-	end
-
-	do -- io
-		local io = {}
-
-		local META = {}
-		META.__index = META
-
-		function META:__tostring()
-			return ("file (%p)"):format(self)
-		end
-
-		function META:write(...)
-
-			local str = ""
-
-			for i = 1, select("#", ...) do
-				str = str .. tostring(select(i, ...))
-			end
-
-			self.__data = str
-
-			if self.__path:sub(0, 5) == "data/" then
-				file.Write(handle_path(self.__path:sub(6)), self.__data, "DATA")
-			else
-				file.Write(handle_path(self.__path), self.__data, "DATA")
-			end
-		end
-
-		local function read(self, format)
-			format = format or "*line"
-
-			self.__seekpos = self.__seekpos or 0
-
-			if type(format) == "number" then
-				return self.__data:sub(0, format)
-			elseif format:sub(1, 2) == "*a" then
-				return self.__data:sub(self.__seekpos)
-			elseif format:sub(1, 2) == "*l" then
-				if not self.__data:find("\n", nil, true) then
-					if self.__data == "" then return nil end
-
-					local str = self.__data
-					self.__data = ""
-
-					return str
-				else
-					local val = self.__data:match("(.-)\n")
-					self.__data = self.__data:match(".-\n(.+)")
-
-					return val
-				end
-			elseif format:sub(1, 2) == "*n" then
-				local str = read("*line")
-				if str then
-					local numbers = {}
-					str:gsub("(%S+)", function(str) table.insert(numbers, tonumber(str)) end)
-					return unpack(numbers)
-				end
-			end
-		end
-
-		function META:read(...)
-			local args = {...}
-
-			for k, v in pairs(args) do
-				args[k] = read(self, v) or nil
-			end
-
-			return unpack(args) or nil
-		end
-
-		function META:close()
-
-		end
-
-		function META:flush()
-
-		end
-
-		function META:seek(whence, offset)
-			whence = whence or "cur"
-			offset = offset or 0
-
-			self.__seekpos = self.__seekpos or 0
-
-			if whence == "set" then
-				self.__seekpos = offset
-			elseif whence == "end" then
-				self.__seekpos = #self.__data
-			elseif whence == "cur" then
-				self.__seekpos = self.__seekpos + offset
-			end
-
-			return math.Clamp(self.__seekpos, 0, #self.__data)
-		end
-
-		function META:lines()
-			return self.__data:gmatch("(.-)\n")
-		end
-
-		function META:setvbuf()
-
-		end
-
-		function io.open(filename, mode)
-			mode = mode or "r"
-
-			if not file.Exists(handle_path(filename), "DATA") and mode == "w" then
-				error("No such file or directory", 2)
-			end
-
-			local self = setmetatable({}, META)
-
-			self.__data = file.Read(handle_path(filename), "DATA")
-			self.__path = filename
-			self.__mode = mode
-
-			return self
-		end
-
-		io.stdin = setmetatable({}, META)
-		io.stdin.__data = ""
-
-		io.stdout = setmetatable({}, META)
-		io.stdout.__data = ""
-
-		local current_file = io.stdin
-
-		function io.input(var)
-			if io.type(var) == "file" then
-				current_file = var
-			else
-				current_file = io.open(var)
-			end
-
-			return current_file
-		end
-
-		function io.type(var)
-			if getmetatable(var) == META then
-				return file
-			end
-
-			return nil
-		end
-
-		function io.read(...) return current_file:read(...) end
-
-		function io.lines(...) return current_file:lines(...) end
-
-		function io.flush(...) return current_file:flush(...) end
-
-		function io.popen(...) print("io.popen: ", ...) end
-
-		function io.close(...) return current_file:close(...) end
-
-		function io.tmpfile(...) return current_file:flush(...)  end
-
-		env.io = io
-	end
-
-	do -- require
-		local stdlibs =
-		{
-			table = table,
-			math = math,
-			string = string,
-			debug = debug,
-			io = io,
-			table = table,
-			os = os,
-		}
-
-		function env.require(str, ...)
-			local args = {pcall(old_gmod_require, str, ...)}
-
-			if args[1] == false then
-				error(args[2],  2)
-			else
-				table.remove(args, 1)
-
-				if args[1] == nil then
-					return stdlibs[str]
-				end
-
-				return unpack(args)
-			end
-		end
-	end
-
-	do -- std lua env
-		local _G = env
-
-		_G._G = _G
-		_G._VERSION = _VERSION
-
-		_G.assert = assert
-		_G.collectgarbage = collectgarbage
-		_G.dofile = dofile
-		_G.error = error
-		_G.getfenv = getfenv
-		_G.getmetatable = getmetatable
-		_G.ipairs = ipairs
-		_G.load = load
-		_G.loadfile = loadfile
-		_G.loadstring = loadstring
-		_G.module = module
-		_G.next = next
-		_G.pairs = pairs
-		_G.pcall = pcall
-		_G.print = _G.print
-		_G.rawequal = rawequal
-		_G.rawget = rawget
-		_G.rawset = rawset
-		_G.require = require
-		_G.select = select
-		_G.setfenv = setfenv
-		_G.setmetatable = setmetatable
-		_G.tonumber = tonumber
-		_G.tostring = tostring
-		_G.type = type
-		_G.unpack = unpack
-		_G.xpcall = xpcall
-
-		_G.coroutine = {}
-		_G.coroutine.create = coroutine.create
-		_G.coroutine.resume = coroutine.resume
-		_G.coroutine.running = coroutine.running
-		_G.coroutine.status = coroutine.status
-		_G.coroutine.wrap = coroutine.wrap
-		_G.coroutine.yield = coroutine.yield
-
-		_G.debug = {}
-		_G.debug.debug = debug.debug
-		_G.debug.getfenv = debug.getfenv
-		_G.debug.gethook = debug.gethook
-		_G.debug.getinfo = debug.getinfo
-		_G.debug.getlocal = debug.getlocal
-		_G.debug.getmetatable = debug.getmetatable
-		_G.debug.getregistry = debug.getregistry
-		_G.debug.getupvalue = debug.getupvalue
-		_G.debug.setfenv = debug.setfenv
-		_G.debug.sethook = debug.sethook
-		_G.debug.setlocal = debug.setlocal
-		_G.debug.setmetatable = debug.setmetatable
-		_G.debug.setupvalue = debug.setupvalue
-		_G.debug.traceback = debug.traceback
-
-		_G.math = {}
-		_G.math.abs = math.abs
-		_G.math.acos = math.acos
-		_G.math.asin = math.asin
-		_G.math.atan = math.atan
-		_G.math.atan2 = math.atan2
-		_G.math.ceil = math.ceil
-		_G.math.cos = math.cos
-		_G.math.cosh = math.cosh
-		_G.math.deg = math.deg
-		_G.math.exp = math.exp
-		_G.math.floor = math.floor
-		_G.math.fmod = math.fmod
-		_G.math.frexp = math.frexp
-		_G.math.huge = math.huge
-		_G.math.ldexp = math.ldexp
-		_G.math.log = math.log
-		_G.math.log10 = math.log10
-		_G.math.max = math.max
-		_G.math.min = math.min
-		_G.math.modf = math.modf
-		_G.math.pi = math.pi
-		_G.math.pow = math.pow
-		_G.math.rad = math.rad
-		_G.math.random = math.random
-		_G.math.randomseed = math.randomseed
-		_G.math.sin = math.sin
-		_G.math.sinh = math.sinh
-		_G.math.sqrt = math.sqrt
-		_G.math.tan = math.tan
-		_G.math.tanh = math.tanh
-
-		_G.package = {}
-		_G.package.cpath = package.cpath
-		_G.package.loaded = package.loaded
-		_G.package.loaders = package.loaders
-		_G.package.loadlib = package.loadlib
-		_G.package.path = package.path
-		_G.package.preload = package.preload
-		_G.package.seeall = package.seeall
-
-		_G.string = {}
-		_G.string.byte = string.byte
-		_G.string.char = string.char
-		_G.string.dump = string.dump
-		_G.string.find = string.find
-		_G.string.format = string.format
-		_G.string.gmatch = string.gmatch
-		_G.string.gsub = string.gsub
-		_G.string.len = string.len
-		_G.string.lower = string.lower
-		_G.string.match = string.match
-		_G.string.rep = string.rep
-		_G.string.reverse = string.reverse
-		_G.string.sub = string.sub
-		_G.string.upper = string.upper
-
-		_G.table = {}
-		_G.table.concat = table.concat
-		_G.table.insert = table.insert
-		_G.table.maxn = table.maxn
-		_G.table.remove = table.remove
-		_G.table.sort = table.sort
-	end
-end
+include("notagain/goluwa/goluwa/std.lua")
 
 env.runfile("goluwa/libraries/extensions/string.lua")
 env.runfile("goluwa/libraries/extensions/table.lua")
@@ -482,7 +87,6 @@ env.runfile("goluwa/libraries/extensions/math.lua")
 env.utf8 = env.runfile("goluwa/libraries/utf8.lua")
 
 local http = table.Copy(http)
-local sound = table.Copy(sound)
 
 do
 	-- just to debug any http requests
@@ -491,6 +95,8 @@ do
 		_G.http.Fetch(url, ...)
 	end
 end
+
+local sound = table.Copy(sound)
 
 if system.IsLinux() then -- sound.PlayFile fix
 	sound.active = {}
@@ -554,14 +160,6 @@ if system.IsLinux() then -- sound.PlayFile fix
 			end
 		end
 	end)
-end
-
-function env.loadstring(str, env)
-	local var = CompileString(str, env or "loadstring", false)
-	if type(var) == "string" then
-		return nil, var, 2
-	end
-	return setfenv(var, getfenv(1))
 end
 
 function env.typex(val)
@@ -1370,8 +968,6 @@ end
 
 env.autocomplete = env.runfile("goluwa/libraries/autocomplete.lua")
 env.chatsounds = env.runfile("goluwa/libraries/audio/chatsounds/chatsounds.lua")
-
-_G.goluwa = env
 
 for k,v in pairs(env.string) do
 	_G.string[k] = _G.string[k] or v
