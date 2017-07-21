77cb8c43df2c68c4be5ec90af7fff97a5a1c9316
CapsAdmin <eliashogstvedt@gmail.com>
2017-07-21 10:09:29 +0200

    fix syntax error and move aowl commands to votes.lua

--- lua/notagain/essential/autorun/votes.lua
+++ lua/notagain/essential/autorun/votes.lua
@@ -224,4 +224,30 @@ if CLIENT then
         end
     end)
 
+	hook.Add("AowlInitialized", "votes", function()
+		aowl.AddCommand("votekick=player,string[bye]",function(ply,line,ent,reason)
+			if cleanup and cleanup.CC_Cleanup then
+				cleanup.CC_Cleanup(ent,"gmod_cleanup",{})
+			end
+
+			votes.Create(ply:Nick().." wants to kick "..ent:Nick(),30,{"yes","no"},function(votes)
+				if votes[1] == "yes" then
+					ent:Kick("kicked by "..ply:Nick())
+				end
+			end)
+		end)
+
+		aowl.AddCommand("vote=string[vote],number[60],string", function(ply, line, str,num,...)
+			votes.Create(str,num,{...})
+			if votes.IsOnGoing() then
+				gamemode.Call("PlayerButtonUp",ply,num+1)
+			end
+		end)
+
+		hook.Remove("AowlInitialized", "votes")
+	end)
+
+	if players.GetAll()[1] then
+		hook.GetTable().AowlInitialized.votes()
+	end
 end
