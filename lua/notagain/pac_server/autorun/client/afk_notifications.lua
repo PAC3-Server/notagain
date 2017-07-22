857678274866b7de5f174bc82a1e5f1716e2b7ac
R.Réno <reno.reboursier@gmail.com>
2017-07-22 19:22:26 +0200

    more "watchable" colors

--- lua/notagain/pac_server/autorun/client/afk_notifications.lua
+++ lua/notagain/pac_server/autorun/client/afk_notifications.lua
@@ -1,15 +1,15 @@
 hook.Add("OnPlayerAFK", "afk_notifications", function(ply, afk, time)
 	if ply:GetFriendStatus() == "friend" then
 		if afk then
-			chat.AddText(Color(209,62,57),"⮞ ",Color(200,200,200),ply:GetName().." is now ",Color(209,62,57),"away")
+			chat.AddText(Color(255,127,127),"⮞ ",Color(200,200,200),ply:GetName().." is now ",Color(255,127,127),"away")
 		else
-			chat.AddText(Color(92,184,92),"⮞ ",Color(200,200,200),ply:GetName().." is now ",Color(92,184,92),"back",Color(175,175,175)," (away for "..string.NiceTime(time)..")")
+			chat.AddText(Color(127,255,127),"⮞ ",Color(200,200,200),ply:GetName().." is now ",Color(127,255,127),"back",Color(175,175,175)," (away for "..string.NiceTime(time)..")")
 		end
 	elseif ply == LocalPlayer() then
 		if afk then
-			chat.AddText(Color(209,62,57),"⮞ ",Color(200,200,200),"You are now ",Color(209,62,57),"away")
+			chat.AddText(Color(255,127,127),"⮞ ",Color(200,200,200),"You are now ",Color(255,127,127),"away")
 		else
-			chat.AddText(Color(92,184,92),"⮞ Welcome Back!",Color(200,200,200)," You were away for ",Color(175,175,175),string.NiceTime(time))
+			chat.AddText(Color(127,255,127),"⮞ Welcome Back!",Color(200,200,200)," You were away for ",Color(175,175,175),string.NiceTime(time))
 		end
 	end
 end)
