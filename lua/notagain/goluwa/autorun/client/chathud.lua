a15ea1cf3f48320c339ed860321a9f65c82cca94
R.Réno <reno.reboursier@gmail.com>
2017-07-22 21:49:12 +0200

    hihi

--- lua/notagain/goluwa/autorun/client/chathud.lua
+++ lua/notagain/goluwa/autorun/client/chathud.lua
@@ -9,7 +9,7 @@ chathud.default_font = {
 	size = 18,
 	weight = 600,
 	blur_size = 3,
-	background_color = Color(0,0,0,255),--Color(25,50,100,255),
+	background_color = Color(127,127,170),
 	blur_overdraw = 3,
 }
 
