7d3e5aa2da51433201dc5b0f507bc6f8120306d5
Earu <reno.reboursier@gmail.com>
2017-07-24 22:23:17 +0200

    asdasd

--- lua/notagain/jrpg/aowl_commands/alerts.lua
+++ lua/notagain/jrpg/aowl_commands/alerts.lua
@@ -4,9 +4,11 @@ if SERVER then
 	util.AddNetworkString("jalert")
 
 	aowl.AddCommand("alert|jalert|psa=string,number[0]", function(ply, line, message, delay)
+		if not message then return end
+		local delay = delay or 10
 		net.Start("jalert")
 			net.WriteString(message)
-			net.WriteDouble(delay)
+			net.WriteInt(delay,32)
 		net.Broadcast()
 	end, "developers")
 end
@@ -14,7 +16,9 @@ end
 if CLIENT then
 	net.Receive("jalert",function()
 		local message = net.ReadString()
-		local delay = net.ReadDouble()
-		JAlert.DoAlert(message, delay)
+		local delay = net.ReadInt(32)
+		if JAlert then
+			JAlert.DoAlert(message, delay)
+		end
 	end)
 end
