4bce21f64144026a350842ee02a7ac9d3e738531
Earu <reno.reboursier@gmail.com>
2017-07-25 18:38:53 +0200

    donut forget that

--- lua/notagain/aowl/commands/nickname.lua
+++ lua/notagain/aowl/commands/nickname.lua
@@ -20,6 +20,7 @@ if SERVER then
 	local nick
 
     aowl.AddCommand("name|nick=string[ ]", function(caller, line)
+		if not IsValid(caller) or not caller.SetNick then return end
 		local cd = nextChange[caller:UserID()]
 		if cd and cd > CurTime() then
 			return false, "You're changing nicks too quickly!"
