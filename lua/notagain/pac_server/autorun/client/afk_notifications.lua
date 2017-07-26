5d771db585184da7df059cde2c6d35ab83b168db
R.Réno <reno.reboursier@gmail.com>
2017-07-27 01:13:06 +0200

    because nil values??

--- lua/notagain/pac_server/autorun/client/afk_notifications.lua
+++ lua/notagain/pac_server/autorun/client/afk_notifications.lua
@@ -1,5 +1,5 @@
 local notify = function(ply, afk, time)
-	if ply:GetFriendStatus() == "friend" then
+	if IsValid(ply) and ply:GetFriendStatus() == "friend" then
 		if afk then
 			chat.AddText(Color(255,127,127),"⮞ ",Color(200,200,200),ply:GetName().." is now ",Color(255,127,127),"away")
 		else
