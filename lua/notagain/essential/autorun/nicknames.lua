6120ba68803ee7d373edaaa7ddb07c3cdffb2320
R.Réno <reno.reboursier@gmail.com>
2017-07-26 21:23:11 +0200

    dont need that anymore

--- lua/notagain/essential/autorun/nicknames.lua
+++ lua/notagain/essential/autorun/nicknames.lua
@@ -17,38 +17,17 @@ PLAYER.GetName = PLAYER.Nick
 
 if SERVER then
 
-	local validname = function(name)
-		local fuckthose = {
-			["font"] = true,
-			["hsv"] = true,
-			["translate"] = true,
-			["rotate"] = true,
-			["blackhole"] = true,
-		}
-		for k,v in pairs(fuckthose) do
-			if string.match(string.lower(name),"<"..k) then
-				return false
-			end
-		end
-		return true
-	end
-
 	PLAYER.SetNick = function(self,nick)
 		local proper,_ = string.gsub((nick or ""),"<.->","")
 		if not nick or string.TrimLeft(proper) == "" then
 			self:SetPData("Nick","")
 		else
-			if validname(nick) then
-				self:SetPData("Nick", nick)
-			else
-				self:SetPData("Nick","")
-				nick = ""
-			end
+			self:SetPData("Nick", nick)
 		end
 		self:SetNWString("Nick", nick)
 	end
 
-    hook.Add("PlayerInitialSpawn", "nicknames", function(ply)
+	hook.Add("PlayerInitialSpawn", "nicknames", function(ply)
 		if ply:GetPData("Nick") and string.TrimLeft(ply:GetPData("Nick")) ~= "" then
 			ply:SetNick(ply:GetPData("Nick"))
 		end
