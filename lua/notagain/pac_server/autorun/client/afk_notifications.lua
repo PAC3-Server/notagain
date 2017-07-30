57ac86d6d210e14ab2b4c401b1f69ac6673e0e3e
R.Réno <reno.reboursier@gmail.com>
2017-07-30 12:43:14 +0200

    Update afk_notifications.lua

--- lua/notagain/pac_server/autorun/client/afk_notifications.lua
+++ lua/notagain/pac_server/autorun/client/afk_notifications.lua
@@ -2,9 +2,9 @@ local notify = function(ply, afk, time)
 	if not IsValid(ply) then return end
 	if ply:GetFriendStatus() == "friend" then
 		if afk then
-			chat.AddText(Color(255,127,127),"⮞ ",Color(200,200,200),ply:GetName().." is now ",Color(255,127,127),"away")
+			chat.AddText(Color(255,127,127),"⮞ ",Color(200,200,200),ply:GetProperName().." is now ",Color(255,127,127),"away")
 		else
-			chat.AddText(Color(127,255,127),"⮞ ",Color(200,200,200),ply:GetName().." is now ",Color(127,255,127),"back",Color(175,175,175)," (away for "..string.NiceTime(time)..")")
+			chat.AddText(Color(127,255,127),"⮞ ",Color(200,200,200),ply:GetProperName().." is now ",Color(127,255,127),"back",Color(175,175,175)," (away for "..string.NiceTime(time)..")")
 		end
 	elseif ply == LocalPlayer() then
 		if afk then
