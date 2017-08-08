9cd2c33387036fdb0fb2e4080616f04d8c65b2cc
R.Réno <reno.reboursier@gmail.com>
2017-08-08 17:29:18 +0200

    tweaking colors

--- lua/notagain/jrpg/autorun/client/area_notifications.lua
+++ lua/notagain/jrpg/autorun/client/area_notifications.lua
@@ -16,7 +16,7 @@ local CreatePanel = function(area)
     panel:SetSize(500,70)
     panel:SetPos(ScrW(),y_pos)
     panel.Paint = function(self,w,h)
-        surface.SetDrawColor(0,0,0,250)
+        surface.SetDrawColor(30,30,30,255)
         surface.DrawPoly({
             { x = w*0.2, y = 0 },
             { x = w, y = 0 },
