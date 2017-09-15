8edb32cf8fe6fabcbff7dac71973f3670b293867
CapsAdmin <eliashogstvedt@gmail.com>
2017-09-15 21:40:42 +0200

    auto restart

---
+++ lua/notagain/pac_server/autorun/server/auto_restart.lua
@@ -0,0 +1,43 @@
+local restarting = false
+local try_restart = false
+
+timer.Create("auto_restart", 2, 0, function()
+	local want_restart = os.time() - tonumber(file.Read("server_last_restart", "DATA") or 0) > (3*60*60)
+
+	if file.Exists("server_want_restart", "DATA") then
+		try_restart = true
+		file.Delete("server_want_restart")
+	end
+
+	if want_restart or try_restart then
+		local players = player.GetAll()
+		local afk = true
+
+		for _, ply in ipairs(players) do
+			afk = ply:IsAFK()
+			if not afk then
+				break
+			end
+		end
+
+		if afk then
+			if not players[1] then
+				game.ConsoleCommand("changelevel " .. game.GetMap() .. "\n")
+				return
+			end
+
+			if not restarting then
+				restarting = true
+				aowl.CountDown(15, "RESTARTING SERVER BECAUSE EVERYONE IS AFK", function()
+					file.Write("server_last_restart", tostring(os.time()))
+					game.ConsoleCommand("changelevel " .. game.GetMap() .. "\n")
+				end)
+			end
+		else
+			if restarting then
+				restarting = false
+				aowl.AbortCountDown()
+			end
+		end
+	end
+end)
