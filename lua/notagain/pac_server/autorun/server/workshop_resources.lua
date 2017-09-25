186f16eab6b5814c407fb4973ac0e8fd8cdedd6f
CapsAdmin <eliashogstvedt@gmail.com>
2017-09-25 13:02:41 +0200

    move custom workshop content to servercontent addon

--- lua/notagain/pac_server/autorun/server/workshop_resources.lua
+++ lua/notagain/pac_server/autorun/server/workshop_resources.lua
@@ -1,5 +1,4 @@
 resource.AddWorkshop("546392647") -- Media Player
-resource.AddWorkshop("879302614") -- cuustom server content
 resource.AddWorkshop("104482086") -- Precision Tool (Not on Server).
 resource.AddWorkshop("1132466603") -- StormFox.
 
