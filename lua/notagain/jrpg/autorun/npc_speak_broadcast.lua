7ae850934a723a0c826998c85aa1dbf29cf5ac2e
CapsAdmin <eliashogstvedt@gmail.com>
2017-09-30 00:39:10 +0200

    call NPCSpeak for clientside sounds as well and only if channel is voice

--- lua/notagain/jrpg/autorun/npc_speak_broadcast.lua
+++ lua/notagain/jrpg/autorun/npc_speak_broadcast.lua
@@ -1,15 +1,18 @@
-if SERVER then
-	hook.Add("EntityEmitSound","npc_speak_broadcast", function(data)
-		if not data.Entity:IsNPC() then return end
+hook.Add("EntityEmitSound", "npc_speak_broadcast", function(data)
+	if not data.Entity:IsNPC() or data.Channel ~= 2 then return end
 
+	if SERVER then
 		net.Start("npc_speak_broadcast")
 			net.WriteEntity(data.Entity)
 			net.WriteString(data.OriginalSoundName)
 		net.Broadcast()
-	end)
+	else
+		hook.Run("NPCSpeak", data.Entity, data.OriginalSoundName)
+	end
+end)
 
+if SERVER then
 	util.AddNetworkString("npc_speak_broadcast")
-
 end
 
 if CLIENT then
