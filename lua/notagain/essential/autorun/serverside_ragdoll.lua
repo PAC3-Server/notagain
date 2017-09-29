2a4673470d7677927fb225daea469ae48e158ee3
CapsAdmin <eliashogstvedt@gmail.com>
2017-09-29 14:21:18 +0200

    fix GetRagdollEntity and whatnot not working serverside

--- lua/notagain/essential/autorun/serverside_ragdoll.lua
+++ lua/notagain/essential/autorun/serverside_ragdoll.lua
@@ -1,26 +1,26 @@
+local META = FindMetaTable("Player")
+META.Old_GetRagdollEntity = META.Old_GetRagdollEntity or META.GetRagdollEntity
+
+function META:GetRagdollEntity(...)
+	local ent = self:GetNWEntity("serverside_ragdoll")
+	if ent:IsValid() then
+		return ent
+	end
+	return META.Old_GetRagdollEntity(self, ...)
+end
+
+local META = FindMetaTable("Entity")
+META.Old_GetRagdollOwner = META.Old_GetRagdollOwner or META.GetRagdollOwner
+
+function META:GetRagdollOwner(...)
+	local ply = self:GetOwner()
+	if ply:IsValid() and ply:IsPlayer() and ply:GetRagdollEntity() == self then
+		return ply
+	end
+	return META.Old_GetRagdollOwner(self, ...)
+end
+
 if CLIENT then
-	local META = FindMetaTable("Player")
-	META.Old_GetRagdollEntity = META.Old_GetRagdollEntity or META.GetRagdollEntity
-
-	function META:GetRagdollEntity(...)
-		local ent = self:GetNWEntity("serverside_ragdoll")
-		if ent:IsValid() then
-			return ent
-		end
-		return META.Old_GetRagdollEntity(self, ...)
-	end
-
-	local META = FindMetaTable("Entity")
-	META.Old_GetRagdollOwner = META.Old_GetRagdollOwner or META.GetRagdollOwner
-
-	function META:GetRagdollOwner(...)
-		local ply = self:GetOwner()
-		if ply:IsValid() and ply:IsPlayer() and ply:GetRagdollEntity() == self then
-			return ply
-		end
-		return META.Old_GetRagdollOwner(self, ...)
-	end
-
 	hook.Add("CalcView", "serverside_ragdoll", function(ply, origin, angles)
 		local ent = ply:GetNWEntity("serverside_ragdoll")
 		if ent:IsValid() then
