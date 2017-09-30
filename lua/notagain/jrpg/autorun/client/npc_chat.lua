25520e0f1f252b36626ac8ad8f27613c4df3de2e
CapsAdmin <eliashogstvedt@gmail.com>
2017-09-30 23:11:40 +0200

    add npc chat

---
+++ lua/notagain/jrpg/autorun/client/npc_chat.lua
@@ -0,0 +1,18 @@
+hook.Add("NPCSpeak", "npc_chat", function(npc, text)
+	if npc:EyePos():Distance(LocalPlayer():EyePos()) > 1500 then return end
+
+	local color
+	if jrpg.IsFriend(LocalPlayer(), npc) then
+		color = team.GetColor(TEAM_FRIENDS)
+	else
+		color = team.GetColor(TEAM_PLAYERS)
+	end
+
+	local str = language.GetPhrase(text)
+	if str and str ~= text then
+		str = str:gsub("%b<>", ""):Trim()
+		if str ~= "" then
+			chat.AddText(color, jrpg.GetFriendlyName(npc), color_white, ": ", str)
+		end
+	end
+end)
