4b95e51c92aaf7e4386f5af2e33fa9d79340d6bd
CapsAdmin <eliashogstvedt@gmail.com>
2017-10-14 14:24:20 +0200

    added dodge

--- lua/notagain/jrpg/autorun/roll.lua
+++ lua/notagain/jrpg/autorun/roll.lua
@@ -3,6 +3,10 @@ local threshold = 30
 
 local roll_time = 0.75/speed
 local roll_speed = 1*speed
+
+local dodge_time = 0.75/speed
+local dodge_speed = 1*speed
+
 local function is_rolling(ply)
 	if CLIENT and ply ~= LocalPlayer() then
 		return ply:GetNW2Float("roll_time", CurTime()) > CurTime()
@@ -10,12 +14,19 @@ local function is_rolling(ply)
 	return ply.roll_time and ply.roll_time > CurTime()
 end
 
-local function vel_to_dir(ang, vel)
+local function is_dodging(ply)
+	if CLIENT and ply ~= LocalPlayer() then
+		return ply:GetNW2Float("dodge_time", CurTime()) > CurTime()
+	end
+	return ply.dodge_time and ply.dodge_time > CurTime()
+end
+
+local function vel_to_dir(ang, vel, speed)
 	ang.p = 0
 	local dot = ang:Forward():Dot(vel)
-	if dot > 100*roll_speed then
+	if dot > 100*speed then
 		return "forward"
-	elseif dot < -100*roll_speed then
+	elseif dot < -100*speed then
 		return "backward"
 	end
 
@@ -31,7 +42,7 @@ if SERVER then
 	util.AddNetworkString("roll")
 
 	hook.Add("EntityTakeDamage", "roll", function(ent, dmginfo)
-		if is_rolling(ent) and (bit.band(dmginfo:GetDamageType(), DMG_CRUSH) > 0 or bit.band(dmginfo:GetDamageType(), DMG_SLASH) > 0) then
+		if (is_rolling(ent) or is_dodging(ent)) and (bit.band(dmginfo:GetDamageType(), DMG_CRUSH) > 0 or bit.band(dmginfo:GetDamageType(), DMG_SLASH) > 0) then
 			dmginfo:ScaleDamage(0)
 			ent:ChatPrint("dodge!")
 			return true
@@ -41,7 +52,7 @@ end
 
 if CLIENT then
 	hook.Add("CalcView", "roll", function(ply, pos, ang)
-		if is_rolling(ply) and ply:Alive() and not ply:InVehicle() and not ply:ShouldDrawLocalPlayer() then
+		if (is_rolling(ply) or is_dodging(ply)) and ply:Alive() and not ply:InVehicle() and not ply:ShouldDrawLocalPlayer() then
 			local eyes = ply:GetAttachment(ply:LookupAttachment("eyes"))
 
 			return {
@@ -55,7 +66,7 @@ if CLIENT then
 		if not wep or not wep:IsValid() then return end
 		local ply = LocalPlayer()
 
-		if is_rolling(ply) and ply:Alive() and not ply:InVehicle() and not ply:ShouldDrawLocalPlayer() then
+		if (is_rolling(ply) or is_dodging(ply)) and ply:Alive() and not ply:InVehicle() and not ply:ShouldDrawLocalPlayer() then
 			local eyes = ply:GetAttachment(ply:LookupAttachment("eyes"))
 
 			return eyes.Pos, eyes.Ang
@@ -68,14 +79,49 @@ local function can_roll(ply)
 	return (ply:IsValid() and ply:GetNWBool("rpg") and not is_rolling(ply) and ply:Alive() and ply:OnGround() and ply:GetMoveType() == MOVETYPE_WALK and not ply:InVehicle()) or ply.roll_landed
 end
 
+local function can_dodge(ply)
+	return (ply:IsValid() and ply:GetNWBool("rpg") and not is_dodging(ply) and ply:Alive() and ply:OnGround() and ply:GetMoveType() == MOVETYPE_WALK and not ply:InVehicle())
+end
+
 hook.Add("UpdateAnimation", "roll", function(ply, velocity)
 	if velocity:Length2D() < threshold then
 		ply.roll_time = nil
 		return
 	end
 
+	if is_dodging(ply) then
+		local dir = vel_to_dir(ply:EyeAngles(), velocity, dodge_speed)
+
+		ply.dodge_back_cycle = (ply.dodge_back_cycle or 0) + (ply:GetVelocity():Length2D() * FrameTime() / 250)
+
+		ply.dodge_fraction = math.Clamp(ply.dodge_back_cycle,0,1)
+
+		if ply.dodge_fraction < 1 then
+
+			local f = ply.dodge_fraction
+			--f = math.EaseInOut(f, 1, 2)
+
+			if dir == "forward" then
+				local cycle = Lerp(f, 0.68, 0.3)
+				if cycle <= 0.31 then cycle = 0.68 end
+				ply:SetCycle(cycle)
+			elseif dir == "backward" then
+				ply:SetCycle(Lerp(f, 0.3, 0.75))
+			elseif dir == "left" or dir == "right" then
+				ply:SetCycle(Lerp(f, 0.25, 1))
+			end
+
+			ply:SetPlaybackRate(0)
+
+			return true
+		end
+	else
+		ply.dodge_back_cycle = nil
+		ply.dodge_fraction = nil
+	end
+
 	if is_rolling(ply) then
-		local dir = vel_to_dir(ply:EyeAngles(), velocity)
+		local dir = vel_to_dir(ply:EyeAngles(), velocity, roll_speed)
 
 		if dir == "forward" or dir == "backward" then
 			ply.roll_back_cycle = (ply.roll_back_cycle or 0) + (ply:GetVelocity():Length2D() * FrameTime() / 200)
@@ -97,7 +143,7 @@ hook.Add("UpdateAnimation", "roll", function(ply, velocity)
 				if cycle < 0.05 then cycle = 1 end
 				ply:SetCycle(cycle)
 			elseif dir == "left" or dir == "right" then
-				ply:SetCycle(Lerp(f, 0.25, 1))
+				ply:SetCycle(Lerp(f, 0, 1))
 			end
 
 			ply:SetPlaybackRate(0)
@@ -123,7 +169,7 @@ hook.Add("CalcMainActivity", "roll", function(ply)
 	end
 
 	if is_rolling(ply) and ply.roll_fraction and ply.roll_fraction < 1 then
-		local dir = vel_to_dir(ply:EyeAngles(), ply:GetVelocity())
+		local dir = vel_to_dir(ply:EyeAngles(), ply:GetVelocity(), roll_speed)
 
 		local seq = ""
 
@@ -141,30 +187,83 @@ hook.Add("CalcMainActivity", "roll", function(ply)
 			return seqid, seqid
 		end
 	end
+
+	if is_dodging(ply) and ply.dodge_fraction and ply.dodge_fraction < 1 then
+		local dir = vel_to_dir(ply:EyeAngles(), ply:GetVelocity(), dodge_speed)
+
+		local seq = "roll_backward"
+
+		if dir == "left" then
+			---seq = "pure_b_s2_t1"
+		elseif dir == "right" then
+		--	seq = "pure_b_s2_t1"
+		end
+
+
+		local seqid = ply:LookupSequence(seq)
+
+		if seqid > 1 then
+			return seqid, seqid
+		end
+	end
 end)
 
 hook.Add("Move", "roll", function(ply, mv, ucmd)
 	if not ply:GetNWBool("rpg") then return end
 
-	--[[local landed
+	if can_dodge(ply) then
+		local stamina = jattributes.GetStamina(ply)
+		if stamina < 30 then return end
 
-	if not ply:OnGround() then
-		ply.roll_in_air = CurTime()
-	else
-		local diff = (CurTime() - ply.roll_in_air)
+		if mv:KeyPressed(IN_JUMP) then
 
-		if diff > 0 and diff < 0.1 then
-			landed = true
+			if (mv:KeyDown(IN_ATTACK) or mv:KeyDown(IN_ATTACK2) or mv:KeyDown(IN_SPEED)) and mv:KeyDown(IN_JUMP) then
+				ply.dodge_ang = mv:GetAngles()
+
+				ply.dodge_time2 = dodge_time / math.Clamp(mv:GetVelocity():Length2D() / 200, 0.75, 1.25)
+				ply.dodge_time = CurTime() + ply.dodge_time2
+			end
+
+			if ply.dodge_time then
+				if mv:GetForwardSpeed() > 0 then
+					ply.dodge_dir = "forward"
+				elseif mv:GetForwardSpeed() < 0 then
+					ply.dodge_dir = "backward"
+				end
+
+				if not ply.dodge_dir then
+					if mv:GetSideSpeed() > 0 then
+						ply.dodge_dir = "right"
+					else
+						ply.dodge_dir = "left"
+					end
+				end
+
+				ply:AnimRestartMainSequence()
+
+				if SERVER then
+					ply:SetNW2Float("dodge_time", ply.dodge_time)
+					ply:SetNW2Float("dodge_time2", ply.dodge_time2)
+
+					jattributes.SetStamina(ply, stamina - 15)
+
+					ply:EmitSound("npc/zombie/foot_slide3.wav")
+				end
+			end
+		else
+			ply.dodge_ang = nil
+			ply.dodge_time = nil
+			ply.dodge_time2 = nil
+			ply.dodge_dir = nil
 		end
 	end
-	]]
 
 	if can_roll(ply) then
+
+		local stamina = jattributes.GetStamina(ply)
+		if stamina < 30 then return end
+
 		if mv:KeyPressed(IN_DUCK) or ply.roll_landed then
-
-			local stamina = jattributes.GetStamina(ply)
-			if stamina < 30 then return end
-
 			if ply.roll_landed then
 				local vel = mv:GetVelocity()
 				local forward = ply:GetForward():Dot(vel)
@@ -221,6 +320,34 @@ hook.Add("Move", "roll", function(ply, mv, ucmd)
 			ply.roll_time2 = nil
 			ply.roll_dir = nil
 		end
+	end
+
+	if is_dodging(ply) and mv:GetVelocity():Length2D() > 30 then
+		local dir
+
+		local ang = ply.dodge_ang * 1
+		ang.p = 0
+
+		local dir = ply.dodge_dir
+		local f = math.Clamp((ply.dodge_time - CurTime()) * (1/ply:GetNW2Float("dodge_time2")), 0, 1)
+
+		local mult = (math.sin(f*math.pi) + 0.5) * dodge_speed
+
+		mv:SetMaxSpeed(200*mult)
+		mv:SetMaxClientSpeed(200*mult)
+
+		if dir == "forward" then
+			mv:SetForwardSpeed(10000)
+		elseif dir == "backward" then
+			mv:SetForwardSpeed(-10000)
+		elseif dir == "left" then
+			mv:SetSideSpeed(-10000)
+		elseif dir == "right" then
+			mv:SetSideSpeed(10000)
+		end
+	else
+		ply.dodge_forward_speed = nil
+		ply.dodge_side_speed = nil
 	end
 
 	if is_rolling(ply) and mv:GetVelocity():Length2D() > 30 then
