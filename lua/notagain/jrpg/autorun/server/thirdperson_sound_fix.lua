e7dc91da080b9624cddf8bc94041e862394a442a
CapsAdmin <eliashogstvedt@gmail.com>
2017-10-14 11:56:20 +0200

    fix weapon going invisible with thirdperson fix

--- lua/notagain/jrpg/autorun/server/thirdperson_sound_fix.lua
+++ lua/notagain/jrpg/autorun/server/thirdperson_sound_fix.lua
@@ -5,11 +5,20 @@ timer.Create("thirdperson_sound_fix", 0.1, 0, function()
 			local ent = ply:GetViewEntity()
 
 			if ent == ply then
-				ply:SetViewEntity(ply:GetActiveWeapon())
+				SafeRemoveEntity(ply.tp_soundfix_dummy)
+
+				ply.tp_soundfix_dummy = ents.Create("prop_dynamic")
+				ply.tp_soundfix_dummy:SetPos(ply:GetPos())
+				ply.tp_soundfix_dummy:SetParent(ply)
+				ply.tp_soundfix_dummy:SetOwner(ply)
+				ply.tp_soundfix_dummy:Spawn()
+
+				ply:SetViewEntity(ply.tp_soundfix_dummy)
 				ply.thirdperson_sound_fix_set = true
 			end
 		else
 			if ply.thirdperson_sound_fix_set then
+				SafeRemoveEntity(ply.tp_soundfix_dummy)
 				ply:SetViewEntity(ply)
 				ply.thirdperson_sound_fix_set = nil
 			end
