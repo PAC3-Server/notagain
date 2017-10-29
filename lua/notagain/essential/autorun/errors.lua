b204a2077b59193869510e25a52f702c638b5600
techbot <techbot121@gmail.com>
2017-10-29 16:15:15 +0100

    better errors

--- lua/notagain/essential/autorun/errors.lua
+++ lua/notagain/essential/autorun/errors.lua
@@ -8,15 +8,70 @@
 -- function api.ErrorNoHalt(...)
 -- function api.error(...)
 
--- todo: formatting and clientside
+-- todo: clientside
+
+local IsOnGithub =  -- todo automate ??????
+{
+    ["notagain"] = true,
+    ["gm-http-discordrelay"] = true,
+    ["easychat"] = true
+}
+
+local function TryGithub(src, line, start, last)
+    local addon = src:match("addons/(.-)/lua/")
+    local path = src:match("/(lua/.+)")
+    if (addon and path) and IsOnGithub[string.lower(addon)] then
+        if start and last then
+            return "Function: https://github.com/PAC3-Server/" .. addon .. "/tree/master/" .. path .. "#L" .. start .. "-L" .. last, "At: https://github.com/PAC3-Server/" .. addon .. "/tree/master/" .. path .. "#L" .. line
+        else
+            return "At: https://github.com/PAC3-Server/" .. addon .. "/tree/master/" .. path .. "#L" .. line
+        end
+    else
+        return "in " .. src .. " at line: " .. line
+    end
+end
+
 if SERVER then
     local old_error = debug.getregistry()[1]
 
     debug.getregistry()[1] = function(...)
-        if epoe then
-            epoe.api.error(debug.traceback())
-        end
-        old_error(...)
-    end -- barebones just so we have something
 
+        local info = debug.getinfo(2)
+        local fname = tostring(info["name"])
+        local src = {TryGithub(tostring(info["short_src"]), tostring(info["currentline"]), tostring(info["linedefined"]), tostring(info["lastlinedefined"]))}
+        local i = 1
+        local lcls = {}
+        while true do
+            local n, v = debug.getlocal(2,i)
+            if ( n == nil ) then break end
+            n = (n == "(*temporary)") and ">>>>>>>>>>" or n
+            lcls[n] = v
+            i = i + 1
+        end
+        local locals = table.ToString(lcls,"Locals",true)
+        local trace = debug.traceback()
+
+        if epoe then
+            local api = epoe.api
+            api.MsgC(Color(255,0,0),"-- [ERROR from ")
+            api.Msg(fname)
+            api.MsgC(Color(255,0,0)," ] --")
+            api.Msg("\n")
+            api.MsgC(Color(0,128,255),src[1])
+            api.Msg("\n")
+            api.MsgC(Color(0,128,255),src[2])
+            api.Msg("\n")
+            api.MsgN(locals)
+            api.error(trace)
+            api.MsgC(Color(255,0,0),"-- [ ] --")
+            api.Msg("\n")
+
+        else
+            print(fname,"\n",src,"\n",locals,"\n",trace) -- fallback????
+        end
+
+        hook.Run("LuaError", {info = info, locals = locals, trace = trace})
+
+        old_error(...) -- compat??
+    end
 end
