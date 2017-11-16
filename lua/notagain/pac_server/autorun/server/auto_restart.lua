7237ee7ff6b15b2df82cf5e61f8aa243b87d0879
techbot <techbot121@gmail.com>
2017-11-16 19:00:13 +0100

    more parentheses

--- lua/notagain/pac_server/autorun/server/auto_restart.lua
+++ lua/notagain/pac_server/autorun/server/auto_restart.lua
@@ -26,7 +26,7 @@ timer.Create("auto_restart", 2, 0, function()
 
 		if afk then
 			if not players[1] then
-				if discordrelay and discordrelay.ready then discordrelay.notify("Auto Restart trigger: " .. (reason and reason or want_restart and "last restart was over 3 hours ago") or "???") end
+				if discordrelay and discordrelay.ready then discordrelay.notify("Auto Restart trigger: " .. (reason and reason or (want_restart and "last restart was over 3 hours ago")) or "???") end
 				file.Write("server_last_restart.txt", tostring(os.time()))
 				game.ConsoleCommand("changelevel " .. game.GetMap() .. "\n")
 				return
@@ -36,7 +36,7 @@ timer.Create("auto_restart", 2, 0, function()
 				restarting = true
 				aowl.CountDown(15, "RESTARTING SERVER BECAUSE EVERYONE IS AFK", function()
 					if discordrelay and discordrelay.ready then discordrelay.notify("Auto Restarting because everyone is afk " ..
-						(reason and "and updates from " .. reason .. " are pending ..." or want_restart and "and last restart was over 3 hours ago")) end
+						(reason and ("and updates from " .. reason .. " are pending ...") or (want_restart and "and last restart was over 3 hours ago")) or "") end
 					file.Write("server_last_restart.txt", tostring(os.time()))
 					game.ConsoleCommand("changelevel " .. game.GetMap() .. "\n")
 				end)
