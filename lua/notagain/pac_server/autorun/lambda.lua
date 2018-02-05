022953fb1395e3fba600ef75b4a67aa8e05f5fc0
CapsAdmin <eliashogstvedt@gmail.com>
2018-02-05 07:43:53 +0100

    enable rpg by default

--- lua/notagain/pac_server/autorun/lambda.lua
+++ lua/notagain/pac_server/autorun/lambda.lua
@@ -1,6 +1,8 @@
 if engine.ActiveGamemode() ~= "lambda" then return end
 
 hook.Add("PlayerSpawn", "pac_server_lambda", function(ply)
+	jrpg.SetRPG(ply, true)
+
 	timer.Simple(0, function()
 		if ply:IsValid() then
 			SafeRemoveEntity(ply.TrackerEntity)
