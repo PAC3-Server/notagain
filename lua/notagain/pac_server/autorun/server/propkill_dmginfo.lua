e284a91e163b160701d71bd0165c975cc8271873
CapsAdmin <eliashogstvedt@gmail.com>
2018-03-01 22:14:07 +0100

    rewrite

---
+++ lua/notagain/pac_server/autorun/server/propkill_dmginfo.lua
@@ -0,0 +1,25 @@
+hook.Add("PhysgunPickup", "propkill_dmginfo", function(ply, ent)
+	ent.propkill_dmginfo_pickup = ply
+end)
+
+hook.Add("PhysgunDrop", "propkill_dmginfo", function(ply, ent)
+	if IsValid(ent.propkill_dmginfo_pickup) then
+		ent.propkill_dmginfo_dropdata = {ply = ply, when = CurTime() + 3}
+	end
+
+	ent.propkill_dmginfo_pickup = nil
+end)
+
+hook.Add("EntityTakeDamage", "propkill_dmginfo", function(ply, dmginfo)
+	local attacker  = dmginfo:GetAttacker()
+
+	if attacker.propkill_dmginfo_dropdata then
+		local real_attacker = attacker.propkill_dmginfo_dropdata.ply
+print(real_attacker)
+		if IsValid(real_attacker) and attacker.propkill_dmginfo_dropdata.when > CurTime() then
+print("!")
+			dmginfo:SetAttacker(real_attacker)
+			dmginfo:SetInflictor(attacker)
+		end
+	end
+end, -math.huge)
