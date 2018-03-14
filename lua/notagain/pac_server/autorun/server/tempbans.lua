86bdfffb68e20b3be19460f2f3d28c96dbff11e8
Techbot <techbot121@gmail.com>
2018-03-14 14:54:38 +0100

    rename hook

--- lua/notagain/pac_server/autorun/server/tempbans.lua
+++ lua/notagain/pac_server/autorun/server/tempbans.lua
@@ -1,3 +1,3 @@
-hook.Add("CheckPassword","no",function(sid, ip, pswd, pswdp, name)
+hook.Add("CheckPassword","xziros",function(sid, ip, pswd, pswdp, name)
     if sid == "76561198111171443" then return false,"#GameUI_DisconnectConfirmationText" end
 end)
