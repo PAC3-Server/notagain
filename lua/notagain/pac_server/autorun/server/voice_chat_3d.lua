9cab1d36ec1db36d05dc6f68e61027e9cf4fb6df
NanoAi <tenshi@mirai.red>
2018-03-24 20:48:32 -0400

    3DVoice now takes walls into account
    It also changes the update delay based on the number of players.
    I guess it's kinda realistic?

--- lua/notagain/pac_server/autorun/server/voice_chat_3d.lua
+++ lua/notagain/pac_server/autorun/server/voice_chat_3d.lua
@@ -1,29 +1,89 @@
-local function traceFromTo(listener, talker)
-    local tr = util.TraceLine({
+local function canHear(listener, talker)
+    local lShootPos = listener:GetShootPos()
+    local tShootPos = talker:GetShootPos()
+
+    local distance = lShootPos:DistToSqr(tShootPos)
+    local wallDepth = 0
+
+    if distance > 360000 then return false end
+
+    local trA = util.TraceLine({
         start = listener:GetShootPos(),
         endpos = talker:GetShootPos(),
         mask = MASK_SHOT
     })
 
-    return tr.Entity == talker
+    local trB = {}
+
+    if trA.Entity == talker then
+        return true
+    end
+
+    if distance > 40000 then return false end
+
+    trB = util.TraceLine({
+        start = talker:GetShootPos(),
+        endpos = trA.HitPos,
+        filter = talker,
+        mask = MASK_VISIBLE
+    })
+
+    if trB.Entity == listener then
+        return true
+    end
+
+    wallDepth = trA.HitPos:DistToSqr(trB.HitPos)
+    return ( wallDepth < 100 )
 end
 
 local function calcPlyCanHearPlayerVoice(listener)
-    if not IsValid(listener) then return end
-
     listener.CanHear = listener.CanHear or {}
     local shootPos = listener:GetShootPos()
 
     for _, talker in ipairs( player.GetAll() ) do
         local talkerShootPos = talker:GetShootPos()
-        listener.CanHear[talker] = (shootPos:DistToSqr(talkerShootPos) < 360000 and traceFromTo(listener, talker))
+        listener.CanHear[talker] = canHear(listener, talker)
     end
 end
 
-timer.Create("PlayerVoiceCalc", 0.5, 0, function()
+local lastDelay = 0.1
+local playerCount = player.GetCount()
+
+local function timerFunction()
     for _,v in ipairs( player.GetAll() ) do
         calcPlyCanHearPlayerVoice(v)
     end
+end
+
+local function scaleDelay()
+    playerCount = player.GetCount()
+    local scale = playerCount/10
+
+    if scale < 1 and lastDelay ~= 0.1 then
+        timer.Remove("PlayerVoiceCalc")
+        timer.Create("PlayerVoiceCalc", 0.1, 0, timerFunction)
+
+        lastDelay = 0.1
+        print("[3dVoice] Delay Scaled to "..lastDelay)
+    elseif scale > 1 and scale < 3 and lastDelay ~= 0.25 then
+        timer.Remove("PlayerVoiceCalc")
+        timer.Create("PlayerVoiceCalc", 0.25, 0, timerFunction)
+
+        lastDelay = 0.25
+        print("[3dVoice] Delay Scaled to "..lastDelay)
+    elseif scale > 3 and lastDelay ~= 0.5 then
+        timer.Remove("PlayerVoiceCalc")
+        timer.Create("PlayerVoiceCalc", 0.5, 0, timerFunction)
+
+        lastDelay = 0.5
+        print("[3dVoice] Delay Scaled to "..lastDelay)
+    end
+end
+
+scaleDelay()
+
+hook.Add("PlayerInitialSpawn", "PlayerVoiceCalc", function()
+    scaleDelay()
 end)
 
 hook.Add("PlayerDisconnected", "PlayerVoiceCalc", function(ply)
@@ -32,6 +92,8 @@ hook.Add("PlayerDisconnected", "PlayerVoiceCalc", function(ply)
         if not v.CanHear then continue end
         v.CanHear[ply] = nil
     end
+
+    scaleDelay()
 end)
 
 hook.Add("PlayerCanHearPlayersVoice", "voice_chat_3d", function(listener, talker)
