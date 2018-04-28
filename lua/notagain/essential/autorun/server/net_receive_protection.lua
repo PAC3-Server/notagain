e82168198b162d000ecab07ff8747004969a034d
CapsAdmin <eliashogstvedt@gmail.com>
2018-04-28 17:02:09 +0200

    add rate limiting

--- lua/notagain/essential/autorun/server/net_receive_protection.lua
+++ lua/notagain/essential/autorun/server/net_receive_protection.lua
@@ -2,13 +2,45 @@ if SERVER then
 	local SysTime = SysTime
 	local RealTime = RealTime
 
+	local function warn(ply, fmt, ...)
+		ply.net_incoming_last_print_suppress = ply.net_incoming_last_print_suppress or {}
+		ply.net_incoming_last_print_suppress[fmt] = ply.net_incoming_last_print_suppress[fmt] or RealTime()
+
+		if ply.net_incoming_last_print_suppress[fmt] < RealTime() then
+			ply.net_incoming_last_print_suppress[fmt] = RealTime() + 0.5
+
+			fmt = fmt:Replace("PLAYER", ply:Nick() .. "( " .. ply:SteamID() .. " )")
+			local str = fmt:format(...)
+			MsgN("[net] " .. str)
+		end
+	end
+
+	local function punish(ply, sec)
+		ply.net_incoming_suppress = SysTime() + sec
+		warn(ply, "dropping net messages from PLAYER for %f seconds", sec)
+	end
+
 	function net.Incoming(length, ply)
-		if (ply.net_incoming_last_error or 0) + 1 > SysTime() then
-			if (ply.net_incoming_last_print_suppress or 0) < RealTime() then
-				MsgN(("suppressing net message from %s (%s) because of lua error less than a second ago"):format(tostring(ply), ply:SteamID()))
-				ply.net_incoming_last_print_suppress = RealTime() + 1
+		do -- rate limit
+			ply.net_incoming_rate_count = (ply.net_incoming_rate_count or 0) + 1
+			ply.net_incoming_rate_next_check = ply.net_incoming_rate_next_check or 0
+
+			if ply.net_incoming_rate_next_check < RealTime() then
+				if ply.net_incoming_rate_count > 100 then
+					warn(ply, "PLAYER is sending more than 100 net messages a second", ply)
+					punish(ply, 2)
+				end
+
+				ply.net_incoming_rate_count = 0
+				ply.net_incoming_rate_next_check = RealTime() + 1
 			end
-		else
+		end
+
+		if ply.net_incoming_suppress and ply.net_incoming_suppress > SysTime() then
+			return
+		end
+
+		do -- gmod's net.Incoming
 			local i = net.ReadHeader()
 			local id = util.NetworkIDToString(i)
 
@@ -26,7 +58,7 @@ if SERVER then
 					)
 
 					if not ok then
-						ply.net_incoming_last_error = SysTime()
+						punish(ply, 1)
 					end
 				end
 			end
