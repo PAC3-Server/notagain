1cfe2055666423efaa63a41d86ea0d5cce8dd2d6
Elias Hogstvedt <eliashogstvedt@gmail.com>
2020-05-11 00:02:39 +0200

    spawn env_skypaint

--- lua/notagain/pac_server/autorun/server/bluehills_skybox.lua
+++ lua/notagain/pac_server/autorun/server/bluehills_skybox.lua
@@ -1,3 +1,15 @@
 if game.GetMap() ~= "gm_bluehills_test3" then return end
 
-RunConsoleCommand("sv_skyname", "painted")
+hook.Add("InitPostEntity", "bluehills_grass", function()
+	local ent = ents.Create("env_skypaint")
+    ent:Spawn()
+    ent:Activate()
+
+    ent:SetKeyValue("sunposmethod", "0")
+    ent:SetKeyValue("drawstars", "1")
+    ent:SetKeyValue("startexture", "skybox/starfield")
+
+    RunConsoleCommand("sv_skyname", "painted")
+
+    hook.Remove("InitPostEntity", "bluehills_grass")
+end)
